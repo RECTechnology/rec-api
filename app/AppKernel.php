7a6c7415a21c4f9432110a177b4d6ab247a9a8e3
Lluis Santos <cto@arbafinternational.com>
2014-06-08 15:52:44 +0200

    installed jms serializer and nelmio api doc bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Arbaf\FinancialApiBundle\ArbafFinancialApiBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
