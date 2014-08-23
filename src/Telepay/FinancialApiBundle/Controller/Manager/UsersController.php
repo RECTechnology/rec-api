a3599792cfef55d423c0fcdd57dc864a540e7c55
Rick Moreno <cto@arbafinternational.com>
2014-08-23 02:00:39 +0200

    fixed bug at creating users, password was storing as a plain text

--- src/Telepay/FinancialApiBundle/Controller/Manager/UsersController.php
+++ src/Telepay/FinancialApiBundle/Controller/Manager/UsersController.php
@@ -2,6 +2,8 @@
 
 namespace Telepay\FinancialApiBundle\Controller\Manager;
 
+use Doctrine\DBAL\DBALException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Telepay\FinancialApiBundle\Controller\BaseApiController;
 use Telepay\FinancialApiBundle\Entity\User;
 use FOS\RestBundle\Controller\Annotations as Rest;
@@ -34,6 +36,11 @@ class UsersController extends BaseApiController
      * @Rest\View
      */
     public function createAction(Request $request){
+        if(!$request->request->has('password'))
+            throw new HttpException(400, "Missing parameter 'password'");
+        $password = $request->get('password');
+        $request->request->remove('password');
+        $request->request->add(array('plain_password'=>$password));
         return parent::createAction($request);
     }
 
