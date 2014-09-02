88c528f3054be5f92a7a6aa821d8e12682a98d49
Rick Moreno <cto@arbafinternational.com>
2014-09-02 19:56:56 +0200

    created inheritance in statistics controller

--- src/Telepay/FinancialApiBundle/Controller/Stats/TestStats.php
+++ src/Telepay/FinancialApiBundle/Controller/Stats/TestStats.php
@@ -11,43 +11,22 @@ namespace Telepay\FinancialApiBundle\Controller\Stats;
 use Symfony\Component\HttpFoundation\Request;
 use Telepay\FinancialApiBundle\Controller\RestApiController;
 
-class TestStats extends RestApiController{
+class TestStats extends BaseStats{
+
+    public function getServiceName(){
+        return 'Test';
+    }
 
     public function transactions(Request $request, $mode = true) {
-
-        if($request->query->has('limit')) $limit = $request->query->get('limit');
-        else $limit = 10;
-
-        if($request->query->has('offset')) $offset = $request->query->get('offset');
-        else $offset = 0;
-
-        $transactionsRepo = $this->get('doctrine_mongodb')
-            ->getManager()
-            ->getRepository('TelepayFinancialApiBundle:Transaction');
-
-        $transactions = $transactionsRepo->findBy(
-            array(
-                'user' => $this->get('security.context')->getToken()->getUser()->getId(),
-                'service' => $this->get('telepay.services')->findByName('Test')->getId(),
-                'mode'=> $mode
-            ),
-            array('timeIn'=>'DESC'),
-            $limit,
-            $offset
-        );
-
-        $view = $this->buildRestView(
-            200,
-            "Request successful",
-            $transactions
-        );
-
-        return $this->handleView($view);
-
+        return parent::transactions($request, $mode);
     }
 
      public function transactionsTest(Request $request, $mode = false) {
-        return $this->transactions($request, $mode);
+         return parent::transactionsTest($request, $mode);
      }
+
+    public function stats(Request $request, $mode = true) {
+        parent::stats($request, $mode);
+    }
 
 }
