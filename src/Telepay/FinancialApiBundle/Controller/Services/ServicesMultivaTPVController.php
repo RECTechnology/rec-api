8bdc141ad04efe61b000d43e44ab0337fbccfdcb
pere <pere@pasproduccions.com>
2014-12-17 13:15:50 +0100

    Testing multiva

--- src/Telepay/FinancialApiBundle/Controller/Services/ServicesMultivaTPVController.php
+++ src/Telepay/FinancialApiBundle/Controller/Services/ServicesMultivaTPVController.php
@@ -115,7 +115,7 @@ class ServicesMultivaTPVController extends FosRestController
 
         $url_base=$this->container->getParameter('api_url');
         $url_notification=$url_base.'/notifications/v1/multiva?tid='.$id;
-die(print_r('caca',true));
+
         //Check if it's a Test or Production transaction
         if($mode=='T'){
             //Constructor in Test mode
