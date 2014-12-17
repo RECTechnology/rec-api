9b63d5955288c5a36d04a68d71ceec0502c83d35
pere <pere@pasproduccions.com>
2014-12-17 19:17:42 +0100

    Testing hal

--- src/Telepay/FinancialApiBundle/Controller/Services/ServicesHalcashController.php
+++ src/Telepay/FinancialApiBundle/Controller/Services/ServicesHalcashController.php
@@ -172,6 +172,8 @@ class ServicesHalcashController extends FosRestController
                 ->getHalcashSend($mode)
                 ->send($params[0],$params[2],$params[3],$params[4],$params[5]);
 
+            die(print_r($datos,true));
+
             $datos=get_object_vars($datos);
             $datos['error']=get_object_vars($datos['errorcode']);
             $datos['ticket']=get_object_vars($datos['halcashticket']);
