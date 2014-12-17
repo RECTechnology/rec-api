2cc7276982734840cc49fcfaaf573c813fbf91a4
pere <pere@pasproduccions.com>
2014-12-17 19:26:29 +0100

    Testing hal

--- src/Telepay/FinancialApiBundle/Controller/Services/ServicesHalcashController.php
+++ src/Telepay/FinancialApiBundle/Controller/Services/ServicesHalcashController.php
@@ -173,12 +173,13 @@ class ServicesHalcashController extends FosRestController
                 ->send($params[0],$params[2],$params[3],$params[4],$params[5]);
 
 
-
+            $rCode=201;
+            $res="HalCash generated successfully";
             /*$datos=get_object_vars($datos);
             $datos['error']=get_object_vars($datos['errorcode']);
             $datos['ticket']=get_object_vars($datos['halcashticket']);*/
 
-            if($datos['error']=='99'){
+           /* if($datos['error']=='99'){
                 $rCode=503;
                 $res="Service temporally unavailable";
             }elseif($datos['error']=='0'){
@@ -187,7 +188,7 @@ class ServicesHalcashController extends FosRestController
             }else{
                 $rCode=503;
                 $res="Service Unavailable.";
-            }
+            }*/
 
 
         }
