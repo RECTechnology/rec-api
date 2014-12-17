3ef496c634ca7eea0f0cec26e6bd15b14a11db61
pere <pere@pasproduccions.com>
2014-12-17 20:20:33 +0100

    Testing hal

--- src/Telepay/FinancialApiBundle/Controller/Services/ServicesHalcashController.php
+++ src/Telepay/FinancialApiBundle/Controller/Services/ServicesHalcashController.php
@@ -172,6 +172,8 @@ class ServicesHalcashController extends FosRestController
                 ->getHalcashSend($mode)
                 ->send($params[0],$params[2],$params[3],$params[4],$params[5]);
 
+            $datos=$datos->EmisionResult;
+
            if($datos->errorcode=='99'){
                 $rCode=503;
                 $res="Service temporally unavailable";
