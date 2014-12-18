4539adb7fdda68498c0819819439ea5024508bdf
pere <pere@pasproduccions.com>
2014-12-18 12:40:07 +0100

    Testing hal

--- src/Telepay/FinancialApiBundle/Controller/Services/ServicesHalcashController.php
+++ src/Telepay/FinancialApiBundle/Controller/Services/ServicesHalcashController.php
@@ -174,7 +174,7 @@ class ServicesHalcashController extends FosRestController
 
             $rCode=201;
             $res="HalCash generated successfully";
-            $datos=get_object_vars($datos);
+            $datos=$datos->EmisionResult;
 
            /*if($datos->errorcode=='99'){
                 $rCode=503;
