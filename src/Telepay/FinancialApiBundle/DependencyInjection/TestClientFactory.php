7a58d5c06ae8d1cd486dfb744e599d719a7e3a6f
Lluis Santos <dev@telepay.net>
2014-12-23 01:22:38 +0100

    implemented AuthorizationTest

--- src/Telepay/FinancialApiBundle/DependencyInjection/TestClientFactory.php
+++ src/Telepay/FinancialApiBundle/DependencyInjection/TestClientFactory.php
@@ -19,9 +19,9 @@ class TestClientFactory {
             'access-key'=>'8eb4763b5feda5a966c6c5749231f45841631f28',
             'access-secret'=>'hqTXol8N2fT7Rx2whVElnV5zbyzoRC8M+EX4G7JJdRA='
         ),
-        'ROLE_USER'=>array(
-            'access-key'=>'96069af2ab5309e649399271b60f4a0c5b617c2e',
-            'access-secret'=>'LN0JMADBHyjX+FUbKig0ibNPEuboJAi3PqO3pBH/tRE='
+        'user_no_services'=>array(
+            'access-key'=>'00d7f2bc9d4a66d6dd0fe964b7f2e9bdb3d4e961',
+            'access-secret'=>'KGxlyCWhk0+Z3U5HhwFB/6JHd9VvMMo9L3X8cOLvzYY='
         ),
     );
 
