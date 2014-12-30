1286ca75e62ba99ac30899e19cd972c5c260cd5d
pere <pere@pasproduccions.com>
2014-12-30 14:39:56 +0100

    Created Paynet Conciliation Command but not configured yet

---
+++ src/Telepay/FinancialApiBundle/Command/PaynetConciliationCommand.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lluis
+ * Date: 7/15/14
+ * Time: 1:27 PM
+ */
+
+namespace Telepay\FinancialApiBundle\Command;
+
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PaynetConciliationCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('paynet:conciliation')
+            ->setDescription('Greet someone')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Who do you want to greet?')
+            ->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        if ($name) {
+            $text = 'Hello '.$name;
+        } else {
+            $text = 'Hello';
+        }
+
+        if ($input->getOption('yell')) {
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+    }
+}
