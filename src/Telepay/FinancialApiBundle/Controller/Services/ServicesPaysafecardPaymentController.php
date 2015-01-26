ff5f81bf30afd8335fa526f6a98c3d8d160254e1
pere <pere@pasproduccions.com>
2015-01-26 19:34:45 +0100

    Create paysafecard V2 and change amount to cents

--- src/Telepay/FinancialApiBundle/Controller/Services/ServicesPaysafecardPaymentController.php
+++ src/Telepay/FinancialApiBundle/Controller/Services/ServicesPaysafecardPaymentController.php
@@ -179,4 +179,162 @@ class ServicesPaysafecardPaymentController extends FOSRestController
         return $this->request($request);
     }
 
+    /**
+     * This method redirect clients to the Paysafecard getaway to finish the payment.
+     *
+     * @ApiDoc(
+     *   section="Paysafecard",
+     *   description="Returns the redirect info.",
+     *   https="true",
+     *   output="Telepay\FinancialApiBundle\Controller\Services",
+     *   statusCodes={
+     *       201="Returned when the request was successful",
+     *   },
+     *   parameters={
+     *      {
+     *          "name"="mtid",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "format"="dd/mm/yyyyThh:mm:ss",
+     *          "description"="Unique Transation id."
+     *      },
+     *      {
+     *          "name"="currency",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="Currency code. ISO-4217. Ex: MXN"
+     *      },
+     *      {
+     *          "name"="amount",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="Amount for the transaction. Eg: 100.00 = 10000."
+     *      },
+     *      {
+     *          "name"="url_success",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="Url if the transaction was completed succesfully."
+     *      },
+     *      {
+     *          "name"="url_fail",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="Url if the transaction was an error."
+     *      },
+     *      {
+     *          "name"="url_notification",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="The url where we notify the success transactions."
+     *      },
+     *      {
+     *          "name"="merchant_client_id",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="The merchant client from Telepay."
+     *      }
+     *   }
+     * )
+     *
+     */
+
+    public function requestV2(Request $request){
+
+        static $paramNames = array(
+            'mtid',
+            'currency',
+            'amount',
+            'url_success',
+            'url_fail',
+            'url_notification',
+            'merchant_client_id'
+        );
+
+        //Get the parameters sent by POST and put them in $params array
+        $params = array();
+        foreach($paramNames as $paramName){
+            if(!$request->request ->has($paramName)){
+                throw new HttpException(400,"Missing parameter '$paramName'");
+            }
+
+            if($request->get($paramName)===''){
+                throw new HttpException(400,"Missing value for '$paramName'");
+            }
+
+            $params[]=$request->get($paramName, 'null');
+        }
+
+        $count=count($paramNames);
+        $paramsMongo=array();
+        for($i=0; $i<$count; $i++){
+            $paramsMongo[$paramNames[$i]]=$params[$i];
+        }
+
+        //Comprobamos modo Test
+        $mode = $request->get('mode');
+        if(!isset($mode)) $mode = 'P';
+
+        //Guardamos la request en mongo
+        $transaction = new Transaction();
+        $transaction->setIp($request->getClientIp());
+        $transaction->setTimeIn(time());
+        $transaction->setService($this->get('telepay.services')->findByName('SafetyPay')->getId());
+        $transaction->setUser($this->get('security.context')->getToken()->getUser()->getId());
+        $transaction->setSentData(json_encode($paramsMongo));
+        $transaction->setMode($mode === 'P');
+
+        $dms = $this->get('doctrine_mongodb')->getManager();
+        $dms->persist($transaction);
+        $id=$transaction->getId();
+
+        //Convertimos de cents a dos decimales
+        $amount=$params[2]/100;
+
+        $url_base=$request->getSchemeAndHttpHost().$request->getBaseUrl();
+
+        $url_notification=$url_base.'/notifications/v1/paysafecard';
+
+        //Check if it's a Test or Production transaction
+        $datos=$this->get('paysafecard.service')->getPaysafecard()-> request($mode,$params[0],$params[1],$amount,$params[3],$params[4],$url_notification,$params[6]);
+
+        //Response
+        if($datos['error_number']==0){
+            $transaction->setSuccessful(true);
+            //$datos['id']=$id;
+            $rCode=201;
+            $resp = new ApiResponseBuilder(
+                201,
+                "Reference created successfully",
+                $datos
+            );
+        }else{
+            $transaction->setSuccessful(false);
+            $rCode=400;
+            $resp = new ApiResponseBuilder(
+                400,
+                "Bad request",
+                $datos
+            );
+        }
+
+        //Guardamos la respuesta
+        $transaction->setReceivedData(json_encode($datos));
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $transaction->setTimeOut(time());
+        $transaction->setCompleted(false);
+
+        $dm->persist($transaction);
+        $dm->flush();
+
+        $view = $this->view($resp, $rCode);
+
+        return $this->handleView($view);
+    }
+
+    public function requestTestV2(Request $request){
+        $request->request->set('mode','T');
+        return $this->requestV2($request);
+    }
+
 }
