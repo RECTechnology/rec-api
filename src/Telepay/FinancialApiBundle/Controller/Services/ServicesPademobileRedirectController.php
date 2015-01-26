44ac8198c110b7dbd34eeca83ad58814dedf98f4
pere <pere@pasproduccions.com>
2015-01-26 18:06:36 +0100

    Create Pademobile v2 and change amount to cents

--- src/Telepay/FinancialApiBundle/Controller/Services/ServicesPademobileRedirectController.php
+++ src/Telepay/FinancialApiBundle/Controller/Services/ServicesPademobileRedirectController.php
@@ -52,7 +52,7 @@ class ServicesPademobileRedirectController extends FosRestController
      *          "name"="amount",
      *          "dataType"="string",
      *          "required"="true",
-     *          "description"="Shopping amount"
+     *          "description"="Shopping amount with two decimals. Eg => 100.00"
      *      }
      *   }
      * )
@@ -140,6 +140,132 @@ class ServicesPademobileRedirectController extends FosRestController
     public function requestTest(Request $request){
         $request->request->set('mode','T');
         return $this->request($request);
+    }
+
+    /**
+     * This method redirect to Pademobile site for finish the payment.
+     *
+     * @ApiDoc(
+     *   section="Pademobile",
+     *   description="Returns a redirect to finish the payment.",
+     *   https="true",
+     *   statusCodes={
+     *       302="Returned request is successful",
+     *   },
+     *   parameters={
+     *      {
+     *          "name"="country",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="Country. Ex: MX"
+     *      },
+     *      {
+     *          "name"="url",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="URL for notification de payment"
+     *      },
+     *      {
+     *          "name"="description",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="Product description"
+     *      },
+     *      {
+     *          "name"="amount",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="Shopping amount in cents. Eg => 100.00= 10000."
+     *      }
+     *   }
+     * )
+     *
+     */
+
+    public function requestV2(Request $request){
+
+        static $paramNames = array(
+            'country',
+            'url',
+            'description',
+            'amount'
+        );
+
+        //Get the parameters sent by POST and put them in a $params array
+        $params = array();
+        foreach($paramNames as $paramName){
+            if(!$request->request ->has($paramName)){
+                throw new HttpException(400,"Missing parameter '$paramName'");
+            }
+
+            if($request->get($paramName)===''){
+                throw new HttpException(400,"Missing value for '$paramName'");
+            }
+
+            $params[]=$request->get($paramName, 'null');
+        }
+
+        $count=count($paramNames);
+        $paramsMongo=array();
+        for($i=0; $i<$count; $i++){
+            $paramsMongo[$paramNames[$i]]=$params[$i];
+        }
+
+        //Comprobamos modo Test
+        $mode=$request->get('mode');
+        if(!isset($mode))   $mode='P';
+
+        //Guardamos la request en mongo
+        $transaction = new Transaction();
+        $transaction->setIp($request->getClientIp());
+        $transaction->setTimeIn(time());
+        $transaction->setService($this->get('telepay.services')->findByName('Pademobile')->getId());
+        $transaction->setUser($this->get('security.context')->getToken()->getUser()->getId());
+        $transaction->setSentData(json_encode($paramsMongo));
+        $transaction->setMode($mode === 'P');
+
+        $dms = $this->get('doctrine_mongodb')->getManager();
+        $dms->persist($transaction);
+        $id=$transaction->getId();
+
+        $url_base=$request->getSchemeAndHttpHost().$request->getBaseUrl();
+
+        $url_notification=$url_base.'/notifications/v1/pademobile?tid='.$id;
+
+        //Convertimos el amount de centimos a decimales para enviarlos a pademobile
+        $amount=$params[3]/100;
+
+        //Constructor
+        $datos = $this->get('pademobile.service')
+            ->getPademobile($mode)
+            ->request($params[0],$url_notification,$params[2],$amount);
+
+        $transaction->setSuccessful(true);
+        $rCode=302;
+        $resp = new ApiResponseBuilder(
+            302,
+            "New Location generated successfully",
+            $datos
+        );
+
+        //Guardamos la respuesta
+        $transaction->setReceivedData(json_encode($datos));
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $transaction->setTimeOut(time());
+        $transaction->setCompleted(false);
+
+        $dm->persist($transaction);
+        $dm->flush();
+
+        $view = $this->view($resp, $rCode);
+
+        return $this->handleView($view);
+
+    }
+
+    public function requestTestV2(Request $request){
+        $request->request->set('mode','T');
+        return $this->requestV2($request);
     }
 
 }
