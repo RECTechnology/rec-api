c19c37fa7048fae1c89622484b141e4407c5136f
Lluis Santos <cto@entropy-factory.com>
2015-03-16 14:35:25 +0100

    cleared some code and changed routes to system/v1/

--- src/Telepay/FinancialApiBundle/Controller/Management/System/SystemController.php
+++ src/Telepay/FinancialApiBundle/Controller/Management/System/SystemController.php
@@ -2,6 +2,7 @@
 
 namespace Telepay\FinancialApiBundle\Controller\Management\Admin;
 
+use Symfony\Component\HttpFoundation\Request;
 use Telepay\FinancialApiBundle\Controller\RestApiController;
 use Telepay\FinancialApiBundle\DependencyInjection\ServicesRepository;
 use Telepay\FinancialApiBundle\Entity\Group;
@@ -10,7 +11,6 @@ use Telepay\FinancialApiBundle\Response\ApiResponseBuilder;
 use Doctrine\DBAL\DBALException;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
     class Resp{
@@ -28,7 +28,7 @@ class SystemController extends RestApiController
     /**
      * @Rest\View()
      */
-    public function load() {
+    public function load(Request $request) {
         $loadArray = sys_getloadavg();
         $resp = new Resp();
         $resp->avg1 = $loadArray[0];
@@ -44,7 +44,7 @@ class SystemController extends RestApiController
     /**
      * @Rest\View()
      */
-    public function cores() {
+    public function cores(Request $request) {
 
         return $this->handleView($this->buildRestView(
             200,
@@ -57,7 +57,7 @@ class SystemController extends RestApiController
     /**
      * @Rest\View()
      */
-    public function mem() {
+    public function mem(Request $request) {
         $out = `free -m | grep Mem: | awk '{print $3"/"$2}'`;
         $all = explode("/", $out);
         $resp = array(
@@ -75,7 +75,7 @@ class SystemController extends RestApiController
     /**
      * @Rest\View()
      */
-    public function net() {
+    public function net(Request $request) {
         $out = `ifstat -i eth0 1 1 | tail -1 | awk '{print $1"/"$2}'`;
         $all = explode("/", $out);
         $resp = array(
@@ -89,4 +89,18 @@ class SystemController extends RestApiController
         ));
     }
 
+
+    /**
+     * @Rest\View
+     */
+    public function version(Request $request){
+        return $this->rest(
+            200,
+            "Version info got successfully",
+            array(
+                'build_id'  => $this->container->getParameter('build_id'),
+                'version'  => $this->container->getParameter('version')
+            )
+        );
+    }
 }
