1a84278e4462934e076de97fe30ee41e4f7dc932
Lluis Santos <cto@entropy-factory.com>
2015-03-19 23:37:58 +0100

    rollback

--- src/Telepay/FinancialApiBundle/DependencyInjection/Transactions/CryptoPaymentService.php
+++ src/Telepay/FinancialApiBundle/DependencyInjection/Transactions/CryptoPaymentService.php
@@ -35,7 +35,7 @@ class CryptoPaymentService extends BaseService {
 
         //if(!is_int($amount)) throw new HttpException(400, "Amount must be an integer");
         //if(!is_int($confirmations)) throw new HttpException(400, "Confirmations must be an integer");
-        if($amount <= 0 || bcmod($amount, 1) > 0) throw new HttpException(400, "Amount must be positive");
+        if($amount <= 0) throw new HttpException(400, "Amount must be positive");
         if($confirmations < 0 ) throw new HttpException(400, "Confirmation number can't be negative");
 
         $address = $this->cryptoProvider->getnewaddress();
