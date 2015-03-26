c430c74bfc01ef8aff7a9f3ce2799b0a9a7b4f16
pere <pere@pasproduccions.com>
2015-03-26 23:25:08 +0100

    Added transaction controller to superadmin (delete transaction)

---
+++ src/Telepay/FinancialApiBundle/Controller/Management/Admin/TransactionsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Telepay\FinancialApiBundle\Controller\Management\Admin;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Telepay\FinancialApiBundle\Controller\BaseApiController;
+use Telepay\FinancialApiBundle\Controller\RestApiController;
+use Telepay\FinancialApiBundle\Document\Transaction;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class TransactionsController
+ * @package Telepay\FinancialApiBundle\Controller\Management\Admin
+ */
+class TransactionsController extends RestApiController {
+
+    
+
+    /**
+     * @Rest\View
+     */
+    public function deleteAction($id){
+
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $trans=$dm->getRepository('TelepayFinancialApiBundle:Transaction')->find($id);
+
+        if(!$trans) throw new HttpException(404,'Not found');
+
+        $dm->remove($trans);
+        $dm->flush();
+
+        return $this->rest(204, "Deleted");
+    }
+
+
+}
