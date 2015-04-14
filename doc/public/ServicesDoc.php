12d415aa1bfd753750eb0b2d5e7b7ab6c09e3a86
Lluis Santos <cto@entropy-factory.com>
2015-04-15 01:20:33 +0200

    removed v2 from fac_pay and btc_pay

--- doc/public/ServicesDoc.php
+++ doc/public/ServicesDoc.php
@@ -24,30 +24,6 @@
  * @api {post} /services/v1/btc_pay Bitcoin pay
  * @apiName BtcPayCreate
  * @apiDescription Creates an new Bitcoin payment
- * @apiVersion 0.1.0
- * @apiGroup Services
- * @apiUse OAuth2Header
- * @apiParam {Integer} amount The amount in <code>satoshis</code>
- * @apiParam {Integer} confirmations The minimum confirmations to validate the payment
- * @apiParam {Integer} [notification_url] The notification <code>url</code> to notify about the payment result
- * @apiSuccess {Integer} code The HTTP code of the result
- * @apiSuccess {String} message The message about the result of the request
- * @apiSuccess {Object} data The data of the request
- * @apiSuccess {Integer} data.id The transaction <code>id</code>
- * @apiSuccess {Integer} data.amount The amount to send in <code>satoshis</code>
- * @apiSuccess {String} data.address The bitcoin address to send the money
- * @apiSuccess {Integer} data.min_confirmations The minimum confirmations to validate the payment
- * @apiSuccess {Integer} data.expires_in The timeout to expire the transaction if no paid is detected before
- * @apiUse NotAuthenticated
- * @apiUse Error503
- *
- */
-
-/**
- *
- * @api {post} /services/v2/btc_pay Bitcoin pay
- * @apiName BtcPayCreate
- * @apiDescription Creates an new Bitcoin payment
  * @apiVersion 0.2.0
  * @apiGroup Services
  * @apiUse OAuth2Header
@@ -74,30 +50,6 @@
  * @api {get} /services/v1/btc_pay/:id Bitcoin check
  * @apiName BtcPayCheck
  * @apiDescription Checks a Bitcoin transaction
- * @apiVersion 0.1.0
- * @apiGroup Services
- * @apiUse OAuth2Header
- * @apiParam {String} id The transaction <code>id</code>
- * @apiSuccess {Integer} code The HTTP code of the result
- * @apiSuccess {Integer} message The message about the result of the request
- * @apiSuccess {Object} data The data of the request
- * @apiSuccess {Integer} data.id The transaction <code>id</code>
- * @apiSuccess {Integer} data.amount The amount to send in <code>satoshis</code>
- * @apiSuccess {Integer} data.received The received <code>satoshis</code>
- * @apiSuccess {String} data.address The bitcoin address to send the money
- * @apiSuccess {Integer} data.min_confirmations The minimum confirmations to validate the payment
- * @apiSuccess {Integer} data.confirmations The confirmations seen in the payment, if the payment has not seen yet,
- * it returns <code>-1</code>
- * @apiSuccess {Integer} data.expires_in The timeout to expire the transaction if no paid is detected before
- * @apiUse NotAuthenticated
- *
- *
- */
-
-/**
- * @api {get} /services/v2/btc_pay/:id Bitcoin check
- * @apiName BtcPayCheck
- * @apiDescription Checks a Bitcoin transaction
  * @apiVersion 0.2.0
  * @apiGroup Services
  * @apiUse OAuth2Header
@@ -120,33 +72,10 @@
  */
 
 //##################################### FAC_PAY ###################################
-/**
- *
- * @api {post} /services/v1/fac_pay Faircoin pay
- * @apiName FacPayCreate
- * @apiDescription Creates an new Faircoin payment
- * @apiVersion 0.1.0
- * @apiGroup Services
- * @apiUse OAuth2Header
- * @apiParam {Integer} amount The amount in <code>satoshis</code>
- * @apiParam {Integer} confirmations The minimum confirmations to validate the payment
- * @apiParam {Integer} [notification_url] The notification <code>url</code> to notify about the payment result
- * @apiSuccess {Integer} code The HTTP code of the result
- * @apiSuccess {String} message The message about the result of the request
- * @apiSuccess {Object} data The data of the request
- * @apiSuccess {Integer} data.id The transaction <code>id</code>
- * @apiSuccess {Integer} data.amount The amount to send in <code>satoshis</code>
- * @apiSuccess {String} data.address The faircoin address to send the money
- * @apiSuccess {Integer} data.min_confirmations The minimum confirmations to validate the payment
- * @apiSuccess {Integer} data.expires_in The timeout to expire the transaction if no paid is detected before
- * @apiUse NotAuthenticated
- * @apiUse Error503
- *
- */
 
 /**
  *
- * @api {post} /services/v2/fac_pay Faircoin pay
+ * @api {post} /services/v1/fac_pay Faircoin pay
  * @apiName FacPayCreate
  * @apiDescription Creates an new Faircoin payment
  * @apiVersion 0.2.0
@@ -173,30 +102,6 @@
 
 /**
  * @api {get} /services/v1/fac_pay/:id Faircoin check
- * @apiName FacPayCheck
- * @apiDescription Checks a Faircoin transaction
- * @apiVersion 0.1.0
- * @apiGroup Services
- * @apiUse OAuth2Header
- * @apiParam {String} id The transaction <code>id</code>
- * @apiSuccess {Integer} code The HTTP code of the result
- * @apiSuccess {Integer} message The message about the result of the request
- * @apiSuccess {Object} data The data of the request
- * @apiSuccess {Integer} data.id The transaction <code>id</code>
- * @apiSuccess {Integer} data.amount The amount to send in <code>satoshis</code>
- * @apiSuccess {Integer} data.received The received <code>satoshis</code>
- * @apiSuccess {String} data.address The faircoin address to send the money
- * @apiSuccess {Integer} data.min_confirmations The minimum confirmations to validate the payment
- * @apiSuccess {Integer} data.confirmations The confirmations seen in the payment, if the payment has not seen yet,
- * it returns <code>-1</code>
- * @apiSuccess {Integer} data.expires_in The timeout to expire the transaction if no paid is detected before
- * @apiUse NotAuthenticated
- *
- *
- */
-
-/**
- * @api {get} /services/v2/fac_pay/:id Faircoin check
  * @apiName FacPayCheck
  * @apiDescription Checks a Faircoin transaction
  * @apiVersion 0.2.0
