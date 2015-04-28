f7a122a58955221cde22a9aed8fd15a6833fe36e
pere <pere@pasproduccions.com>
2015-04-28 18:44:26 +0200

    Added spei service

---
+++ src/Telepay/FinancialApiBundle/DependencyInjection/Transactions/Libs/SpeiPayment.php
@@ -0,0 +1,57 @@
+<?php
+namespace Telepay\FinancialApiBundle\DependencyInjection\Transactions\Libs;
+
+include('includes/STPServices2.php');
+
+use OrdenPago;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class SpeiPayment{
+
+    private $enterprise;
+    private $account;
+    private $partner;
+    private $operant;
+    private $passphrase;
+
+    function __construct($enterprise, $account, $partner, $operant, $passphrase)
+    {
+        $this->enterprise = $enterprise;
+        $this->account = $account;
+        $this->partner = $partner;
+        $this->operant = $operant;
+        $this->passphrase = $passphrase;
+    }
+
+    public function register($name,$reference,$amount,$transaction_id){
+
+        $data = new OrdenPago();
+        $data->set_empresa($this->enterprise);
+        $data->set_claveRastreo($transaction_id);
+        $data->set_conceptoPago($reference);
+        $data->set_cuentaBeneficiario($this->partner);
+        //$data->set_cuentaOrdenante("846180000050000011");
+        $data->set_referenciaNumerica(2);
+        $data->set_monto($amount);
+        $data->set_tipoCuentaBeneficiario(40);
+        $data->set_tipoPago(1);
+        $data->set_institucionContraparte(846);
+        $data->set_nombreBeneficiario("PERE");
+        $data->set_institucionOperante(90646);
+        //$data->set_iva(16);
+        //$data->set_fechaOperacion('20141112');
+        //$data->set_nombreOrdenante("Juan Lopez");
+        //$data->set_rfcCurpBeneficiario("RFCBEN");
+        //$data->set_rfcCurpOrdenante("RFCORD");
+        //$data->set_tipoCuentaOrdenante(40);
+
+        $pemFile='includes/prueba-key.pem';
+        $passphrase = $this->passphrase;
+
+        $response = registraOrden($data, $pemFile, $passphrase);
+
+        return $response;
+
+    }
+
+}
---
+++ src/Telepay/FinancialApiBundle/DependencyInjection/Transactions/SpeiService.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pere
+ * Date: 11/09/14
+ * Time: 9:58
+ */
+
+namespace Telepay\FinancialApiBundle\DependencyInjection\Transactions;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Telepay\FinancialApiBundle\DependencyInjection\Transactions\Core\BaseService;
+use Telepay\FinancialApiBundle\DependencyInjection\Transactions\Libs\Halcash;
+use Telepay\FinancialApiBundle\Document\Transaction;
+
+
+class SpeiService extends BaseService{
+
+    private $speiProvider;
+
+    public function __construct($name, $cname, $role, $cash_direction, $currency, $base64Image, $speiProvider, $transactionContext){
+        parent::__construct($name, $cname, $role, $cash_direction, $currency, $base64Image, $transactionContext);
+        $this->speiProvider = $speiProvider;
+    }
+
+    public function getFields(){
+        return array(
+            'name', 'reference', 'amount'
+        );
+    }
+
+    public function create(Transaction $baseTransaction = null){
+
+        if($baseTransaction === null) $baseTransaction = new Transaction();
+
+        $reference = $baseTransaction->getDataIn()['reference'];
+        $name = $baseTransaction->getDataIn()['name'];
+        $amount = $baseTransaction->getDataIn()['amount'];
+
+        //pasamos a euros porque lo recibimos en centimos
+        $amount = $amount/100;
+
+        if(strlen($reference) > 210) throw new HttpException(400,'Reference Field must be less than 210 characters');
+
+        $transaction_id = $baseTransaction->getId();
+
+        $stp = $this->speiProvider->register($name,$amount,$reference,$transaction_id);
+
+        if($stp === false)
+            throw new HttpException(503, "Service temporarily unavailable, please try again in a few minutes");
+
+        $baseTransaction->setData($stp);
+        $baseTransaction->setDataOut($stp);
+
+        //todo control de errores
+
+
+        return $baseTransaction;
+
+    }
+
+    public function update(Transaction $transaction, $data){
+
+        return $transaction;
+
+    }
+
+    public function cancel(Transaction $transaction,$data){
+
+        return $transaction;
+
+    }
+
+    public function check(Transaction $transaction){
+
+
+        return $transaction;
+
+    }
+
+}
