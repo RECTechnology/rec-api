d747947095daa5df6cfaac64e9dd6f2555b25826
pere <pere@pasproduccions.com>
2015-04-29 19:21:52 +0200

    Added btc_send and fac_send

---
+++ src/Telepay/FinancialApiBundle/DependencyInjection/Transactions/CryptoSendService.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lluis
+ * Date: 2/25/15
+ * Time: 4:42 AM
+ */
+
+namespace Telepay\FinancialApiBundle\DependencyInjection\Transactions;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Telepay\FinancialApiBundle\DependencyInjection\Telepay\Commons\IntegerManipulator;
+use Telepay\FinancialApiBundle\DependencyInjection\Transactions\Core\BaseService;
+use Telepay\FinancialApiBundle\Document\Transaction;
+
+class CryptoSendService extends BaseService {
+
+    private $cryptoProvider;
+
+    public function __construct($name, $cname, $role, $cash_direction, $currency, $base64Image, $cryptoProvider, $transactionContext){
+        parent::__construct($name, $cname, $role, $cash_direction, $currency, $base64Image, $transactionContext);
+        $this->cryptoProvider = $cryptoProvider;
+    }
+
+    public function getFields(){
+        return array(
+            'amount', 'address'
+        );
+    }
+
+    public function create(Transaction $baseTransaction = null){
+
+        if($baseTransaction === null) $baseTransaction = new Transaction();
+        $amount = $baseTransaction->getDataIn()['amount'];
+        $address = $baseTransaction->getDataIn()['address'];
+
+        $im = new IntegerManipulator();
+        if(!$im->isInteger($amount)) throw new HttpException(400, "amount must be an integer (".$amount.") given");
+        if($amount <= 0) throw new HttpException(400, "Amount must be positive");
+
+        //verify crypto address
+        $address_verification = $this->cryptoProvider->validateaddress($address);
+
+        if(!$address_verification['isvalid']) throw new HttpException(400,'Invalid address.');
+
+        $crypto = $this->cryptoProvider->sendtoaddress($address, $amount/1e8);
+
+        if($crypto === false)
+            throw new HttpException(503, "Service temporarily unavailable, please try again in a few minutes");
+
+        $baseTransaction->setData(array(
+            'id' => $baseTransaction->getId(),
+            'address' => $address,
+            'amount' => doubleval($amount),
+            'txid' => $crypto->txid
+        ));
+
+        $baseTransaction->setDataOut(array(
+            'txid' => $crypto->txid
+        ));
+
+        return $baseTransaction;
+
+    }
+    private function hasExpired($transaction){
+        return $transaction->getTimeIn()->getTimestamp()+$transaction->getData()['expires_in'] < time();
+    }
+
+    public function check(Transaction $transaction){
+
+        $currentData = $transaction->getData();
+
+        if($transaction->getStatus() === 'created' && $this->hasExpired($transaction))
+            $transaction->setStatus('expired');
+
+        if($transaction->getStatus() === 'success' || $transaction->getStatus() === 'expired')
+            return $transaction;
+
+        $address = $currentData['address'];
+        $amount = $currentData['amount'];
+        $allReceived = $this->cryptoProvider->listreceivedbyaddress(0, true);
+        foreach($allReceived as $cryptoData){
+            if($cryptoData['address'] == $address && doubleval($cryptoData['amount'])*1e8 >= $amount){
+                $currentData['received'] = doubleval($cryptoData['amount'])*1e8;
+                $currentData['confirmations'] = $cryptoData['confirmations'];
+                if($currentData['confirmations'] >= $currentData['min_confirmations'])
+                    $transaction->setStatus("success");
+                else
+                    $transaction->setStatus("received");
+                $transaction->setData($currentData);
+                $transaction->setDataOut($currentData);
+                return $transaction;
+            }
+        }
+        return $transaction;
+    }
+
+    public function cancel(Transaction $transaction,$data){
+
+        throw new HttpException(400,'Method not implemented');
+
+    }
+}
