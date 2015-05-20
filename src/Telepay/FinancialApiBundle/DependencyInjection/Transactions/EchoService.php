a2e0a6bc4bcb38b67060d2c28ca6c13bdd19faf2
pere <pere@pasproduccions.com>
2015-05-20 14:13:36 +0200

    Delete use apiDoc

--- src/Telepay/FinancialApiBundle/DependencyInjection/Transactions/EchoService.php
+++ src/Telepay/FinancialApiBundle/DependencyInjection/Transactions/EchoService.php
@@ -7,7 +7,6 @@ use FOS\RestBundle\View\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
