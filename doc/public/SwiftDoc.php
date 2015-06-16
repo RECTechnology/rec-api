d54355f0e0bb4e3bb095cacee33d475cb0248cee
pere <pere@pasproduccions.com>
2015-06-16 14:21:33 +0200

    swift Doc

--- doc/public/SwiftDoc.php
+++ doc/public/SwiftDoc.php
@@ -10,14 +10,14 @@
 //##################################### HELLO BTC ######################################
 
 /**
- * @api {get} /api/hello Read configuration
+ * @api {get} /api/hello Bitcoin configuration
  * @apiName GetConfiguration
- * @apiDescription Read configuration service
+ * @apiDescription Read bitcoin configuration service
  * @apiVersion 1.0.0
  * @apiGroup Swift
- * @apiSuccess {Decimal} price Bitcoin price in EUR
- * @apiSuccess {Decimal} variable_fee Variable fee in parts per unit
- * @apiSuccess {Decimal} fixed_fee Fixed fee in EUR
+ * @apiSuccess {Float} price Bitcoin price in EUR
+ * @apiSuccess {Float} variable_fee Variable fee in parts per unit
+ * @apiSuccess {Float} fixed_fee Fixed fee in EUR
  * @apiSuccess {Integer} timeout Expiration time for the transaction
  * @apiSuccess {Integer} daily_limit Day limit
  * @apiSuccess {Integer} monthly_limit Month limit
@@ -28,57 +28,57 @@
  */
 
 /**
- * @api {get} /api/v2/hello Read configuration
+ * @api {get} /api/v2/hello Bitcoin configuration
  * @apiName GetConfiguration
- * @apiDescription Read configuration service
+ * @apiDescription Read bitcoin configuration service
  * @apiVersion 2.0.0
  * @apiGroup Swift
  * @apiSuccess price Bitcoin prices
- * @apiSuccess price.EUR In EUR
- * @apiSuccess price.PLN In PLN
- * @apiSuccess variable_fee Variable fee in parts per unit
- * @apiSuccess fixed_fee Fixed fee in EUR
- * @apiSuccess timeout Expiration time for the transaction
- * @apiSuccess daily_limit Day limit
- * @apiSuccess monthly_limit Month limit
- * @apiSuccess confirmations Minimum number of confirmations
+ * @apiSuccess {Float} price.EUR In EUR
+ * @apiSuccess {Float} price.PLN In PLN
+ * @apiSuccess {Float} variable_fee Variable fee in parts per unit
+ * @apiSuccess {Float} fixed_fee Fixed fee in EUR
+ * @apiSuccess {Integer} timeout Expiration time for the transaction
+ * @apiSuccess {Integer} daily_limit Day limit
+ * @apiSuccess {Integer} monthly_limit Month limit
+ * @apiSuccess {Integer} confirmations Minimum number of confirmations
  * @apiSuccess {String} terms Url where you can find the terms and conditions
  * @apiSuccess {String} title App name
  *
  */
 
 /**
- * @api {get} /api/v3/hello Read configuration
+ * @api {get} /api/v3/hello Bitcoin configuration
  * @apiName GetConfiguration
- * @apiDescription Read configuration service
+ * @apiDescription Read bitcoin configuration service
  * @apiVersion 3.0.0
  * @apiGroup Swift
  * @apiSuccess price Bitcoin prices
- * @apiSuccess price.eur In EUR
- * @apiSuccess price.pln In PLN
- * @apiSuccess price.mxn In MXN
+ * @apiSuccess {Float} price.eur In EUR
+ * @apiSuccess {Float} price.pln In PLN
+ * @apiSuccess {Float} price.mxn In MXN
  * @apiSuccess limits Limits per currency
- * @apiSuccess limits.day Day limits
- * @apiSuccess limits.day.EUR For EUR
- * @apiSuccess limits.day.PLN For PLN
- * @apiSuccess limits.month Month limits
- * @apiSuccess limits.month.EUR For EUR
- * @apiSuccess limits.month.PLN For PLN
+ * @apiSuccess {Integer} limits.day Day limits
+ * @apiSuccess {Integer} limits.day.EUR For EUR
+ * @apiSuccess {Integer} limits.day.PLN For PLN
+ * @apiSuccess {Integer} limits.month Month limits
+ * @apiSuccess {Integer} limits.month.EUR For EUR
+ * @apiSuccess {Integer} limits.month.PLN For PLN
  * @apiSuccess values Allowed values
- * @apiSuccess values.EUR For EUR transactions
- * @apiSuccess values.PLN For PLN transactions
- * @apiSuccess values.MXN For MXN transactions
+ * @apiSuccess {Integer[]} values.EUR For EUR transactions. Array of integers
+ * @apiSuccess {Integer[]} values.PLN For PLN transactions. Array of integers
+ * @apiSuccess {Integer[]} values.MXN For MXN transactions. Array of integers
  * @apiSuccess fees Fees per currency
- * @apiSuccess fees.fixed Fixed fees
- * @apiSuccess fees.fixed.EUR In EUR
- * @apiSuccess fees.fixed.PLN In PLN
- * @apiSuccess fees.fixed.MXN In MXN
+ * @apiSuccess {Float} fees.fixed Fixed fees
+ * @apiSuccess {Float} fees.fixed.EUR In EUR
+ * @apiSuccess {Float} fees.fixed.PLN In PLN
+ * @apiSuccess {Float} fees.fixed.MXN In MXN
  * @apiSuccess fees.variable Variable fees in parts per unit
- * @apiSuccess fees.variable.EUR For EUR
- * @apiSuccess fees.variable.PLN For PLN
- * @apiSuccess fees.variable.MXN For MXN
- * @apiSuccess timeout Expiration time for the transaction
- * @apiSuccess confirmations Minimum number of confirmations
+ * @apiSuccess {Float} fees.variable.EUR For EUR
+ * @apiSuccess {Float} fees.variable.PLN For PLN
+ * @apiSuccess {Float} fees.variable.MXN For MXN
+ * @apiSuccess {Integer} timeout Expiration time for the transaction
+ * @apiSuccess {Integer} confirmations Minimum number of confirmations
  * @apiSuccess {String} terms Url where you can find the terms and conditions
  * @apiSuccess {String} title App name
  *
@@ -114,10 +114,169 @@
  * @apiDescription Check a BTC transaction by transaction_id
  * @apiVersion 1.0.0
  * @apiGroup Swift
- * @apiSuccess status The resulting status of the transaction
- * @apiSuccess confirmations Number of confirmations
- * @apiSuccess btc Transaction amount in BTC
- * @apiSuccess expired True or false
- * @apiSuccess ticket_id Transaction ticket id
+ * @apiSuccess {String} status The resulting status of the transaction
+ * @apiSuccess {Integer} confirmations Number of confirmations
+ * @apiSuccess {Integer} btc Transaction amount in <code>satoshis</code>
+ * @apiSuccess {Boolean} expired True or false
+ * @apiSuccess {String} ticket_id Transaction ticket id
+ *
+ */
+
+//##################################### HELLO FAC ######################################
+
+/**
+ * @api {get} /api/fac/hello Faircoin configuration
+ * @apiName GetConfiguration
+ * @apiDescription Read faircoin configuration service
+ * @apiVersion 1.0.0
+ * @apiGroup Swift
+ * @apiSuccess {Float} price Faircoin price in EUR
+ * @apiSuccess {Float} variable_fee Variable fee in parts per unit
+ * @apiSuccess {Float} fixed_fee Fixed fee in EUR
+ * @apiSuccess {Integer} timeout Expiration time for the transaction
+ * @apiSuccess {Float} daily_limit Day limit
+ * @apiSuccess {Float} monthly_limit Month limit
+ * @apiSuccess {Integer} confirmations Minimum number of confirmations
+ * @apiSuccess {String} terms Url where you can find the terms and conditions
+ * @apiSuccess {String} title App name
+ *
+ */
+
+/**
+ * @api {get} /api/v2/fac/hello Faircoin configuration
+ * @apiName GetConfiguration
+ * @apiDescription Read faircoin configuration service
+ * @apiVersion 2.0.0
+ * @apiGroup Swift
+ * @apiSuccess price Faircoin prices
+ * @apiSuccess {Float} price.EUR In EUR
+ * @apiSuccess {Float} price.PLN In PLN
+ * @apiSuccess {Float} variable_fee Variable fee in parts per unit
+ * @apiSuccess {Float} fixed_fee Fixed fee in EUR
+ * @apiSuccess {Integer} timeout Expiration time for the transaction
+ * @apiSuccess {Float} daily_limit Day limit
+ * @apiSuccess {Float} monthly_limit Month limit
+ * @apiSuccess {Integer} confirmations Minimum number of confirmations
+ * @apiSuccess {String} terms Url where you can find the terms and conditions
+ * @apiSuccess {String} title App name
+ *
+ */
+
+
+/**
+ * @api {get} /api/v3/fac/hello Faircoin configuration
+ * @apiName GetConfiguration
+ * @apiDescription Read faircoin configuration service
+ * @apiVersion 3.0.0
+ * @apiGroup Swift
+ * @apiSuccess price Faircoin prices
+ * @apiSuccess {Float} price.eur In EUR
+ * @apiSuccess {Float} price.pln In PLN
+ * @apiSuccess {Float} price.mxn In MXN
+ * @apiSuccess limits Limits per currency
+ * @apiSuccess limits.day Day limits
+ * @apiSuccess {Float} limits.day.EUR For EUR
+ * @apiSuccess {Float} limits.day.PLN For PLN
+ * @apiSuccess limits.month Month limits
+ * @apiSuccess {Float} limits.month.EUR For EUR
+ * @apiSuccess {Float} limits.month.PLN For PLN
+ * @apiSuccess values Allowed values
+ * @apiSuccess {Integer[]} values.EUR For EUR transactions
+ * @apiSuccess {Integer[]} values.PLN For PLN transactions
+ * @apiSuccess {Integer[]} values.MXN For MXN transactions
+ * @apiSuccess fees Fees per currency
+ * @apiSuccess fees.fixed Fixed fees
+ * @apiSuccess {Float} fees.fixed.EUR In EUR
+ * @apiSuccess {Float} fees.fixed.PLN In PLN
+ * @apiSuccess {Float} fees.fixed.MXN In MXN
+ * @apiSuccess fees.variable Variable fees in parts per unit
+ * @apiSuccess {Float} fees.variable.EUR For EUR
+ * @apiSuccess {Float} fees.variable.PLN For PLN
+ * @apiSuccess {Float} fees.variable.MXN For MXN
+ * @apiSuccess {Integer} timeout Expiration time for the transaction
+ * @apiSuccess {Integer} confirmations Minimum number of confirmations
+ * @apiSuccess {String} terms Url where you can find the terms and conditions
+ * @apiSuccess {String} title App name
+ *
+ */
+
+//##################################### CREATE FAC ######################################
+
+/**
+ * @api {post} /api/fac/send Faircoin pay
+ * @apiName CreateTransaction
+ * @apiDescription Creates a new FAC transaction
+ * @apiVersion 1.0.0
+ * @apiGroup Swift
+ * @apiParam {String} country Country service ES or PL
+ * @apiParam {Integer} amount Transaction amount in EUR
+ * @apiParam {Integer} phone_number Phone number to send Hal
+ * @apiParam {Integer} prefix Prefix phone number
+ * @apiSuccess {String} status The resulting status of the transaction
+ * @apiSuccess {String} id The ID of the transaction
+ * @apiSuccess {String} address FAC address where the user must send the FAC
+ * @apiSuccess {String} amount FAC transaction amount in <code>microFaircoin</code>
+ * @apiSuccess {Integer} pin Number with four digits
+ * @apiSuccess {String} ticket_id Transaction ticket id
+ *
+ */
+
+//##################################### CHECK FAC ######################################
+
+/**
+ * @api {get} /api/fac/check/:id Faircoin check
+ * @apiName GetTransaction
+ * @apiDescription Check a FAC transaction by transaction_id
+ * @apiVersion 1.0.0
+ * @apiGroup Swift
+ * @apiSuccess {String} status The resulting status of the transaction
+ * @apiSuccess {Integer} confirmations Number of confirmations
+ * @apiSuccess {Integer} fac Transaction amount in FAC
+ * @apiSuccess {Boolean} expired True or false
+ * @apiSuccess {String} ticket_id Transaction ticket id
+ *
+ */
+
+//##################################### CREATE PAYNET/BTC ######################################
+
+/**
+ * @api {post} /api/paynet/btc Paynet-Bitcoin
+ * @apiName CreatePaynetTransaction
+ * @apiDescription Creates a new Paynet-Btc transaction
+ * @apiVersion 1.0.0
+ * @apiGroup Swift
+ * @apiParam {String} description Transaction description
+ * @apiParam {Integer} amount Transaction amount in MXN
+ * @apiParam {Integer} btc_address Valid bitcoin address
+ * @apiSuccess {String} status The resulting status of the transaction
+ * @apiSuccess {String} id The ID of the transaction
+ * @apiSuccess {String} amount Amount in MXN
+ * @apiSuccess {String} barcode String to generate the barcode
+ * @apiSuccess {String} url Url to show the barcode
+ * @apiSuccess {String} expiration_date Expiration date for the payment
+ * @apiSuccess {String} description Your own description sent before
+ * @apiSuccess {String} ticket_id Transaction ticket id
+ *
+ */
+
+//##################################### CREATE PAYNET/FAC ######################################
+
+/**
+ * @api {post} /api/fac/paynet Paynet-Faircoin
+ * @apiName CreatePaynetTransaction
+ * @apiDescription Creates a new Paynet-Fac transaction
+ * @apiVersion 1.0.0
+ * @apiGroup Swift
+ * @apiParam {String} description Transaction description
+ * @apiParam {Integer} amount Transaction amount in MXN
+ * @apiParam {Integer} fac_address Valid bitcoin address
+ * @apiSuccess {String} status The resulting status of the transaction
+ * @apiSuccess {String} id The ID of the transaction
+ * @apiSuccess {String} amount Amount in MXN
+ * @apiSuccess {String} barcode String to generate the barcode
+ * @apiSuccess {String} url Url to show the barcode
+ * @apiSuccess {String} expiration_date Expiration date for the payment
+ * @apiSuccess {String} description Your own description sent before
+ * @apiSuccess {String} ticket_id Transaction ticket id
  *
  */
