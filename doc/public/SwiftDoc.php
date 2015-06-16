0ac2a9bbeb08c3b18062a8986da0bef0769fa16f
pere <pere@pasproduccions.com>
2015-06-16 12:57:17 +0200

    swift Doc

--- doc/public/SwiftDoc.php
+++ doc/public/SwiftDoc.php
@@ -15,15 +15,15 @@
  * @apiDescription Read configuration service
  * @apiVersion 1.0.0
  * @apiGroup Swift
- * @apiSuccess price
- * @apiSuccess variable_fee
- * @apiSuccess fixed_fee
- * @apiSuccess timeout
- * @apiSuccess daily_limit
- * @apiSuccess monthly_limit
- * @apiSuccess confirmations
- * @apiSuccess terms
- * @apiSuccess title
+ * @apiSuccess {Decimal} price Bitcoin price in EUR
+ * @apiSuccess {Decimal} variable_fee Variable fee in parts per unit
+ * @apiSuccess {Decimal} fixed_fee Fixed fee in EUR
+ * @apiSuccess {Integer} timeout Expiration time for the transaction
+ * @apiSuccess {Integer} daily_limit Day limit
+ * @apiSuccess {Integer} monthly_limit Month limit
+ * @apiSuccess {Integer} confirmations Minimum number of confirmations
+ * @apiSuccess {String} terms Url where you can find the terms and conditions
+ * @apiSuccess {String} title App name
  *
  */
 
@@ -33,17 +33,17 @@
  * @apiDescription Read configuration service
  * @apiVersion 2.0.0
  * @apiGroup Swift
- * @apiSuccess price
- * @apiSuccess price.EUR
- * @apiSuccess price.PLN
- * @apiSuccess variable_fee
- * @apiSuccess fixed_fee
- * @apiSuccess timeout
- * @apiSuccess daily_limit
- * @apiSuccess monthly_limit
- * @apiSuccess confirmations
- * @apiSuccess terms
- * @apiSuccess title
+ * @apiSuccess price Bitcoin prices
+ * @apiSuccess price.EUR In EUR
+ * @apiSuccess price.PLN In PLN
+ * @apiSuccess variable_fee Variable fee in parts per unit
+ * @apiSuccess fixed_fee Fixed fee in EUR
+ * @apiSuccess timeout Expiration time for the transaction
+ * @apiSuccess daily_limit Day limit
+ * @apiSuccess monthly_limit Month limit
+ * @apiSuccess confirmations Minimum number of confirmations
+ * @apiSuccess {String} terms Url where you can find the terms and conditions
+ * @apiSuccess {String} title App name
  *
  */
 
@@ -53,34 +53,34 @@
  * @apiDescription Read configuration service
  * @apiVersion 3.0.0
  * @apiGroup Swift
- * @apiSuccess price
- * @apiSuccess price.eur
- * @apiSuccess price.pln
- * @apiSuccess price.mxn
- * @apiSuccess limits
- * @apiSuccess limits.day
- * @apiSuccess limits.day.EUR
- * @apiSuccess limits.day.PLN
- * @apiSuccess limits.month
- * @apiSuccess limits.month.EUR
- * @apiSuccess limits.month.PLN
- * @apiSuccess values
- * @apiSuccess values.EUR
- * @apiSuccess values.PLN
- * @apiSuccess values.MXN
- * @apiSuccess fees
- * @apiSuccess fees.fixed
- * @apiSuccess fees.fixed.EUR
- * @apiSuccess fees.fixed.PLN
- * @apiSuccess fees.fixed.MXN
- * @apiSuccess fees.variable
- * @apiSuccess fees.variable.EUR
- * @apiSuccess fees.variable.PLN
- * @apiSuccess fees.variable.MXN
- * @apiSuccess timeout
- * @apiSuccess confirmations
- * @apiSuccess terms
- * @apiSuccess title
+ * @apiSuccess price Bitcoin prices
+ * @apiSuccess price.eur In EUR
+ * @apiSuccess price.pln In PLN
+ * @apiSuccess price.mxn In MXN
+ * @apiSuccess limits Limits per currency
+ * @apiSuccess limits.day Day limits
+ * @apiSuccess limits.day.EUR For EUR
+ * @apiSuccess limits.day.PLN For PLN
+ * @apiSuccess limits.month Month limits
+ * @apiSuccess limits.month.EUR For EUR
+ * @apiSuccess limits.month.PLN For PLN
+ * @apiSuccess values Allowed values
+ * @apiSuccess values.EUR For EUR transactions
+ * @apiSuccess values.PLN For PLN transactions
+ * @apiSuccess values.MXN For MXN transactions
+ * @apiSuccess fees Fees per currency
+ * @apiSuccess fees.fixed Fixed fees
+ * @apiSuccess fees.fixed.EUR In EUR
+ * @apiSuccess fees.fixed.PLN In PLN
+ * @apiSuccess fees.fixed.MXN In MXN
+ * @apiSuccess fees.variable Variable fees in parts per unit
+ * @apiSuccess fees.variable.EUR For EUR
+ * @apiSuccess fees.variable.PLN For PLN
+ * @apiSuccess fees.variable.MXN For MXN
+ * @apiSuccess timeout Expiration time for the transaction
+ * @apiSuccess confirmations Minimum number of confirmations
+ * @apiSuccess {String} terms Url where you can find the terms and conditions
+ * @apiSuccess {String} title App name
  *
  */
 
@@ -97,27 +97,27 @@
  * @apiParam {Integer} amount Transaction amount in EUR
  * @apiParam {Integer} phone_number Phone number to send Hal
  * @apiParam {Integer} prefix Prefix phone number
- * @apiSuccess status
- * @apiSuccess id
- * @apiSuccess address
- * @apiSuccess amount
- * @apiSuccess pin
- * @apiSuccess ticket_id
+ * @apiSuccess {String} status The resulting status of the transaction
+ * @apiSuccess {String} id The ID of the transaction
+ * @apiSuccess {String} address BTC address where the user must send the BTC
+ * @apiSuccess {String} amount Btc transaction amount in <code>satoshis</code>
+ * @apiSuccess {Integer} pin Number with four digits
+ * @apiSuccess {String} ticket_id Transaction ticket id
  *
  */
 
 //##################################### CHECK BTC ######################################
 
 /**
- * @api {get} /api/check/{id} Bitcoin check
+ * @api {get} /api/check/:id Bitcoin check
  * @apiName GetTransaction
  * @apiDescription Check a BTC transaction by transaction_id
  * @apiVersion 1.0.0
  * @apiGroup Swift
- * @apiSuccess status
- * @apiSuccess confirmations
- * @apiSuccess btc
- * @apiSuccess expired
- * @apiSuccess ticket_id
+ * @apiSuccess status The resulting status of the transaction
+ * @apiSuccess confirmations Number of confirmations
+ * @apiSuccess btc Transaction amount in BTC
+ * @apiSuccess expired True or false
+ * @apiSuccess ticket_id Transaction ticket id
  *
  */
