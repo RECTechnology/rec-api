1da964da21c6fb23e6da5617740da4ff0f011da8
Lluis Santos <cto@entropy-factory.com>
2015-11-12 14:25:39 +0100

    test for jenkins

--- symfony_install.sh
+++ symfony_install.sh
@@ -24,9 +24,7 @@ fi
 cat > app/config/parameters.yml
 
 curl -sS https://getcomposer.org/installer -s | php
-echo "before-composer"
-php composer.phar install --no-scripts --no-dev --optimize-autoloader --quiet
-echo "after-composer"
+php composer.phar install --no-scripts --no-dev --optimize-autoloader #--quiet
 #php vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php
 
 #php app/console doctrine:schema:update --force --env=prod
