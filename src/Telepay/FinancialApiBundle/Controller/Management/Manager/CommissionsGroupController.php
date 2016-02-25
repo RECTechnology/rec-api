3915fc34d954565e15843d3ae7967c5ae3c59845
pere <pere@pasproduccions.com>
2016-02-25 17:09:44 +0100

    negative values not allowed in group fees

--- src/Telepay/FinancialApiBundle/Controller/Management/Manager/CommissionsGroupController.php
+++ src/Telepay/FinancialApiBundle/Controller/Management/Manager/CommissionsGroupController.php
@@ -2,6 +2,7 @@
 
 namespace Telepay\FinancialApiBundle\Controller\Management\Manager;
 
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Telepay\FinancialApiBundle\Controller\BaseApiController;
 use Telepay\FinancialApiBundle\Entity\ServiceFee;
 use FOS\RestBundle\Controller\Annotations as Rest;
@@ -27,6 +28,16 @@ class CommissionsGroupController extends BaseApiController
      * @Rest\View
      */
     public function updateAction(Request $request, $id){
+
+        //TODO negative values not allowed in variable and fixed field
+        if($request->request->has('fixed')){
+            if($request->request->get('fixed') < 0) throw new HttpException(404, 'Parameter fixed must be positive.');
+        }
+
+        if($request->request->has('variable')){
+            if($request->request->get('variable') < 0) throw new HttpException(404, 'Parameter variable must be positive.');
+        }
+
         return parent::updateAction($request, $id);
     }
 
