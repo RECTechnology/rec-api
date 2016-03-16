ec5409598932141d061bd83188df4070726a02c3
pere <pere@pasproduccions.com>
2016-03-16 18:14:37 +0100

    incoming find query modification

--- src/Telepay/FinancialApiBundle/Controller/Transactions/IncomingController2.php
+++ src/Telepay/FinancialApiBundle/Controller/Transactions/IncomingController2.php
@@ -545,7 +545,7 @@ class IncomingController2 extends RestApiController{
 
         $mongo = $this->get('doctrine_mongodb')->getManager();
 
-        $transaction =$mongo->getRepository('TelepayFinancialApiBundle:Transaction')->findOneBy(array(
+        $transaction = $mongo->getRepository('TelepayFinancialApiBundle:Transaction')->findOneBy(array(
             'id'        => $id,
             'method'   =>  $method_cname,
             'user'      =>  $user->getId(),
@@ -678,63 +678,83 @@ class IncomingController2 extends RestApiController{
                 ->field('created')->gte($start_time)
                 ->field('created')->lte($finish_time)
                 ->where("function() {
-            if (typeof this.dataIn !== 'undefined') {
-                if (typeof this.dataIn.phone_number !== 'undefined') {
-                    if(String(this.dataIn.phone_number).indexOf('$search') > -1){
+            if (typeof this.payInInfo !== 'undefined') {
+                if (typeof this.payInInfo.amount !== 'undefined') {
+                    if(String(this.payInInfo.amount).indexOf('$search') > -1){
                         return true;
                     }
                 }
-                if (typeof this.dataIn.address !== 'undefined') {
-                    if(String(this.dataIn.address).indexOf('$search') > -1){
+                if (typeof this.payInInfo.address !== 'undefined') {
+                    if(String(this.payInInfo.address).indexOf('$search') > -1){
                         return true;
                     }
                 }
-                if (typeof this.dataIn.reference !== 'undefined') {
-                    if(String(this.dataIn.reference).indexOf('$search') > -1){
+                if (typeof this.payInInfo.status !== 'undefined') {
+                    if(String(this.payInInfo.status).indexOf('$search') > -1){
                         return true;
                     }
                 }
-                if (typeof this.dataIn.pin !== 'undefined') {
-                    if(String(this.dataIn.pin).indexOf('$search') > -1){
+                if (typeof this.payInInfo.concept !== 'undefined') {
+                    if(String(this.payInInfo.concept).indexOf('$search') > -1){
                         return true;
                     }
                 }
-                if (typeof this.dataIn.order_id !== 'undefined') {
-                    if(String(this.dataIn.order_id).indexOf('$search') > -1){
-                        return true;
-                    }
-                }
+
             }
-            if (typeof this.dataOut !== 'undefined') {
-                if (typeof this.dataOut.transaction_pos_id !== 'undefined') {
-                    if(String(this.dataOut.transaction_pos_id).indexOf('$search') > -1){
+            if (typeof this.payOutInfo !== 'undefined') {
+                if (typeof this.payOutInfo.amount !== 'undefined') {
+                    if(String(this.payOutInfo.amount).indexOf('$search') > -1){
                         return true;
                     }
                 }
-                if (typeof this.dataOut.halcashticket !== 'undefined') {
-                    if(String(this.dataOut.halcashticket).indexOf('$search') > -1){
+                if (typeof this.payOutInfo.halcashticket !== 'undefined') {
+                    if(String(this.payOutInfo.halcashticket).indexOf('$search') > -1){
                         return true;
                     }
                 }
-                if (typeof this.dataOut.txid !== 'undefined') {
-                    if(String(this.dataOut.txid).indexOf('$search') > -1){
+                if (typeof this.payOutInfo.txid !== 'undefined') {
+                    if(String(this.payOutInfo.txid).indexOf('$search') > -1){
                         return true;
                     }
                 }
-                if (typeof this.dataOut.address !== 'undefined') {
-                    if(String(this.dataOut.address).indexOf('$search') > -1){
+                if (typeof this.payOutInfo.address !== 'undefined') {
+                    if(String(this.payOutInfo.address).indexOf('$search') > -1){
                         return true;
                     }
                 }
-                if (typeof this.dataOut.id !== 'undefined') {
-                    if(String(this.dataOut.id).indexOf('$search') > -1){
+                if (typeof this.payOutInfo.concept !== 'undefined') {
+                    if(String(this.payOutInfo.concept).indexOf('$search') > -1){
                         return true;
                     }
                 }
+                if (typeof this.payOutInfo.email !== 'undefined') {
+                    if(String(this.payOutInfo.email).indexOf('$search') > -1){
+                        return true;
+                    }
+                }
+                if (typeof this.payOutInfo.find_token !== 'undefined') {
+                    if(String(this.payOutInfo.find_token).indexOf('$search') > -1){
+                        return true;
+                    }
+                }
+                if (typeof this.payOutInfo.phone !== 'undefined') {
+                    if(String(this.payOutInfo.phone).indexOf('$search') > -1){
+                        return true;
+                    }
+                }
+                if (typeof this.payOutInfo.pin !== 'undefined') {
+                    if(String(this.payOutInfo.pin).indexOf('$search') > -1){
+                        return true;
+                    }
+                }
+
             }
             if ('$search') {
                 if(typeof this.status !== 'undefined' && String(this.status).indexOf('$search') > -1){ return true;}
                 if(typeof this.service !== 'undefined' && String(this.service).indexOf('$search') > -1){ return true;}
+                if(typeof this.method !== 'undefined' && String(this.method).indexOf('$search') > -1){ return true;}
+                if(typeof this.methodIn !== 'undefined' && String(this.methodIn).indexOf('$search') > -1){ return true;}
+                if(typeof this.methodOut !== 'undefined' && String(this.methodOut).indexOf('$search') > -1){ return true;}
                 if(String(this._id).indexOf('$search') > -1){ return true;}
                 return false;
             }
