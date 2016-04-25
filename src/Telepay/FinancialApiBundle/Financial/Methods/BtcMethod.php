cf06973cf14681423b9e5868a3d03f94fe3ba82c
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2016-04-25 20:41:27 +0200

    txid funcionando en btc

--- src/Telepay/FinancialApiBundle/Financial/Methods/BtcMethod.php
+++ src/Telepay/FinancialApiBundle/Financial/Methods/BtcMethod.php
@@ -142,9 +142,8 @@ class BtcMethod extends BaseMethod {
             $paymentInfo['status'] = Transaction::$STATUS_FAILED;
             $paymentInfo['final'] = false;
         }else{
-            $paymentInfo['txid'] = $crypto->txid;
+            $paymentInfo['txid'] = $crypto;
             $paymentInfo['status'] = 'sent';
-            $paymentInfo['info_easybtc'] = json_encode($crypto);
             $paymentInfo['final'] = true;
         }
 
