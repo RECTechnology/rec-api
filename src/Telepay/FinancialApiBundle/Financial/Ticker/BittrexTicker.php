e734017b3ee52b6855af8674b072f964d6c952b0
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2016-05-25 19:23:43 +0200

    precio de faircoin mejorado

--- src/Telepay/FinancialApiBundle/Financial/Ticker/BittrexTicker.php
+++ src/Telepay/FinancialApiBundle/Financial/Ticker/BittrexTicker.php
@@ -20,11 +20,28 @@ class BittrexTicker implements TickerInterface {
         $this->bittrex = $bittrex;
     }
 
-    public function getPrice()
+    public function getPriceOld()
     {
         $resp = $this->bittrex->ticker('BTC-FAIR');
         if($resp->success != 1) throw new \LogicException("Failed getting FAC -> BTC price");
         return $resp->result->Bid;
+    }
+
+    public function getPrice()
+    {
+        $resp = $this->bittrex->getOrderBook('BTC-FAIR');
+        if($resp->success != 1) throw new \LogicException("Failed getting FAC -> BTC price");
+        $sum_btc = 0;
+        $sum_fac = 0;
+        foreach($resp->result->sell as $ask){
+            $sum_btc += $ask->Quantity * $ask->Rate;
+            $sum_fac += $ask->Quantity;
+            //1000 euros son 2.5 btc
+            if($sum_btc>2.5){
+                return $sum_btc/$sum_fac;
+            }
+        }
+        return $sum_btc/$sum_fac;
     }
 
     public function getInCurrency()
