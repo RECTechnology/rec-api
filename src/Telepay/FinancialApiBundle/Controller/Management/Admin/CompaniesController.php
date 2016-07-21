fd7e6b636fc4e311f7c489e63711492b98c64ac2
Pere <pere@pasproduccions.com>
2016-07-21 16:11:44 +0200

    change company creator

--- src/Telepay/FinancialApiBundle/Controller/Management/Admin/CompaniesController.php
+++ src/Telepay/FinancialApiBundle/Controller/Management/Admin/CompaniesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Telepay\FinancialApiBundle\Controller\Management\Manager;
+namespace Telepay\FinancialApiBundle\Controller\Management\Admin;
 
 use Doctrine\DBAL\DBALException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
