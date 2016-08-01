6fad75c89cd4b4d9bbda975e836c3b64697657c3
Pere <pere@pasproduccions.com>
2016-08-01 17:16:03 +0200

    add commIVand to mLiAgrCaHtUeP Afees to feeInfo field

---
+++ src/Telepay/FinancialApiBundle/Command/MigrateFeeInfoCommand.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lluis
+ * Date: 7/15/14
+ * Time: 1:27 PM
+ */
+
+namespace Telepay\FinancialApiBundle\Command;
+
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MigrateFeeInfoCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('telepay:migrate:fee-info')
+            ->setDescription('Migrate feeinfo')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dm = $this->getContainer()->get('doctrine_mongodb')->getManager();
+
+        $transactions = $dm->getRepository("TelepayFinancialApiBundle:Transaction")->findBy(array(
+            'type'  =>  'fee'
+        ));
+
+        $output->writeln('Migrating '.count($transactions).' transactions...');
+        $counterTransactions = 0;
+        foreach($transactions as $transaction){
+            $transaction->setFeeInfo($transaction->getDataIn());
+            $dm->persist($transaction);
+            $dm->flush($transaction);
+            $counterTransactions ++;
+        }
+
+        $output->writeln($counterTransactions.' transactions updated');
+
+        $output->writeln('All done');
+    }
+
+}
