feb2789094fba6f7e7711879827cab818d1b2209
Pere <pere@pasproduccions.com>
2016-08-12 19:08:57 +0200

    new feature: deposits and fixed address for easypay, sepa, bitcoin and faircoin

--- src/Telepay/FinancialApiBundle/Controller/Management/Admin/SpecialActionsController.php
+++ src/Telepay/FinancialApiBundle/Controller/Management/Admin/SpecialActionsController.php
@@ -79,7 +79,7 @@ class SpecialActionsController extends RestApiController {
 
         //Create cash in transaction
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $fee_manipulator = $this->get('net.telepay.common.fee_manipulator');
+        $fee_manipulator = $this->get('net.telepay.commons.fee_manipulator');
         $company_fees = $fee_manipulator->getMethodFees($token->getCompany(), $token->getMethod());
         $fixed = $company_fees->getFixed();
         $variable = $params['amount'] * $company_fees->getVariable()/100;
