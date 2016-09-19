1c5e0e4786e4c231646ec2b7bb7a648435ee1e62
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2016-09-19 19:22:29 +0200

    incomingController2 like a service

--- src/Telepay/FinancialApiBundle/Command/CheckScheduledCommand.php
+++ src/Telepay/FinancialApiBundle/Command/CheckScheduledCommand.php
@@ -3,20 +3,11 @@ namespace Telepay\FinancialApiBundle\Command;
 
 use Doctrine\DBAL\Types\ObjectType;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Telepay\FinancialApiBundle\DependencyInjection\Telepay\Commons\FeeDeal;
-use Telepay\FinancialApiBundle\DependencyInjection\Telepay\Commons\BalanceManipulator;
-use Telepay\FinancialApiBundle\DependencyInjection\Telepay\Commons\LimitAdder;
-use Telepay\FinancialApiBundle\Document\Transaction;
-use Telepay\FinancialApiBundle\Entity\Exchange;
 use Telepay\FinancialApiBundle\Entity\Group;
-use Telepay\FinancialApiBundle\Entity\UserWallet;
-use Telepay\FinancialApiBundle\Financial\Currency;
 
 class CheckScheduledCommand extends ContainerAwareCommand{
     protected function configure(){
@@ -50,65 +41,19 @@ class CheckScheduledCommand extends ContainerAwareCommand{
                     $group_fee = $this->_getFees($group, $method);
                     $amount = round(($amount * ((100 - $group_fee->getVariable())/100) - $group_fee->getFixed()),0);
                     $amount = 1000;
+                    $request = Request::create(array(
+                        'amount' => $amount
+                    ));
 
-                    $url = 'https://pre-api.chip-chap.com/methods/v1/out/sepa';
-                    $data = array('key1' => 'value1', 'key2' => 'value2');
-                    $headers = array(
-                        'Content-Type' => 'application/json',
-                        'Cache-Control' => 'no-store',
-                        'Pragma' => 'no-cache',
-                    );
-
-                    $data = $this->call(
-                        $url,
-                        'POST',
-                        array(),
-                        $data,
-                        array('Accept'=>'application/json')
-                    );
-
-                    if(!isset($data->error)){
-                        $output->writeln("GOOD");
-                        $output->writeln(json_encode($data));
-                    }
-                    else{
-                        $output->writeln("ERROR");
-                        $output->writeln(json_encode($data));
-                    }
+                    $this->forward('app.incoming_controller:make', array(
+                        'request' => $request,
+                        'version_number' => 1,
+                        'type' => "out",
+                        'method_cname' => $scheduled->getMethod()
+                    ));
                 }
             }
         }
         $output->writeln('All done');
-    }
-
-    public function call($func, $method, $urlParams = array(), $params = array(), $headers = array()){
-        $ch = curl_init($func.'?'.http_build_query($urlParams));
-
-        $curlHeaders = array();
-        foreach($headers as $key => $value){
-            $curlHeaders []= ucfirst($key).': '.$value;
-        }
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $curlHeaders);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $method = strtoupper($method);
-        switch($method){
-            case "GET":
-                break;
-            case "POST":
-                curl_setopt($ch, CURLOPT_POST, 1);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-                break;
-            case "DELETE":
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-                break;
-            case "PUT":
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-                break;
-        }
-        $response = json_decode(curl_exec($ch));
-        curl_close ($ch);
-        return $response;
     }
 }
