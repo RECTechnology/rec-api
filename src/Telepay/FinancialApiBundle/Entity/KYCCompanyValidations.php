4de8dd79c357ab48fd870d8f68e34fb0e721855d
Pere <pere@pasproduccions.com>
2017-01-02 13:18:53 +0100

    doing tier 2

--- src/Telepay/FinancialApiBundle/Entity/KYCCompanyValidations.php
+++ src/Telepay/FinancialApiBundle/Entity/KYCCompanyValidations.php
@@ -46,27 +46,65 @@ class KYCCompanyValidations
     private $phone;
 
     /**
+     * @ORM\Column(type="boolean" ,nullable=true)
+     * @Expose
+     */
+    private $cif;
+
+    /**
+     * @ORM\Column(type="boolean" ,nullable=true)
+     * @Expose
+     */
+    private $zip;
+
+    /**
+     * @ORM\Column(type="boolean" ,nullable=true)
+     * @Expose
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="boolean" ,nullable=true)
+     * @Expose
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="boolean" ,nullable=true)
+     * @Expose
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="boolean" ,nullable=true)
+     * @Expose
+     */
+    private $town;
+
+    /**
+     * @ORM\Column(type="string", nullable = true)
+     * @Expose
+     */
+    private $tier_2_file;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @Expose
+     */
+    private $tier_2_file_description;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @Expose
+     */
+    private $tier2_status;
+
+    /**
      * @return mixed
      */
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
-
-    /**
-     * @param mixed $user
-     */
-    public function setUser($user)
-    {
-        $this->user = $user;
     }
 
     /**
@@ -99,6 +137,158 @@ class KYCCompanyValidations
     public function setPhone($phone)
     {
         $this->phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * @param mixed $company
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCif()
+    {
+        return $this->cif;
+    }
+
+    /**
+     * @param mixed $cif
+     */
+    public function setCif($cif)
+    {
+        $this->cif = $cif;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getZip()
+    {
+        return $this->zip;
+    }
+
+    /**
+     * @param mixed $zip
+     */
+    public function setZip($zip)
+    {
+        $this->zip = $zip;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param mixed $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTown()
+    {
+        return $this->town;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTier2File()
+    {
+        return $this->tier_2_file;
+    }
+
+    /**
+     * @param mixed $tier_2_file
+     */
+    public function setTier2File($tier_2_file)
+    {
+        $this->tier_2_file = $tier_2_file;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTier2FileDescription()
+    {
+        return $this->tier_2_file_description;
+    }
+
+    /**
+     * @param mixed $tier_2_file_description
+     */
+    public function setTier2FileDescription($tier_2_file_description)
+    {
+        $this->tier_2_file_description = $tier_2_file_description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTier2Status()
+    {
+        return $this->tier2_status;
+    }
+
+    /**
+     * @param mixed $tier2_status
+     */
+    public function setTier2Status($tier2_status)
+    {
+        $this->tier2_status = $tier2_status;
     }
 
 
