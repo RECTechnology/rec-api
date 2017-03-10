bd002d30fd3ff56be4a8d38f094c14c6a95d23e4
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2017-03-10 16:02:50 +0100

    bug in parameter name solved

--- src/Telepay/FinancialApiBundle/Controller/Transactions/IncomingController2.php
+++ src/Telepay/FinancialApiBundle/Controller/Transactions/IncomingController2.php
@@ -1141,7 +1141,7 @@ class IncomingController2 extends RestApiController{
 
             $commerce_client = $this->container->getParameter('commerce_client_id');
             $android_pos_client = $this->container->getParameter('android_pos_client_id');
-            $fairpay_android_pos_client = $this->container->getParameter('fairpay_android_pos_client');
+            $fairpay_android_pos_client = $this->container->getParameter('fairpay_android_pos_client_id');
 
             $client = $accessToken->getClient();
             if($commerce_client == $client->getId() || $android_pos_client == $client->getId() || $fairpay_android_pos_client == $client->getId()){
