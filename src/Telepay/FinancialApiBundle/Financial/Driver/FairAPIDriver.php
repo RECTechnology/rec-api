9d0145c64572dd0feca78e6e4aa30f00adb86178
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2017-04-12 19:49:39 +0200

    clean code in FairAPI

--- src/Telepay/FinancialApiBundle/Financial/Driver/FairAPIDriver.php
+++ src/Telepay/FinancialApiBundle/Financial/Driver/FairAPIDriver.php
@@ -5,98 +5,35 @@ namespace Telepay\FinancialApiBundle\Financial\Driver;
 use ChipChapLL\BaseRequester;
 use ChipChapLL\Core\ApiKey;
 use ChipChapLL\Core\Credentials;
-use TelepayApi\Core\ApiRequest;
 use TelepayApi\Core\JsonRequester;
 
 class FairAPIDriver extends JsonRequester {
-    private $requestBuilder;
-
-    public function __construct($fairAPI_url, $fairAPI_key, $fairAPI_secret) {
-        $this->requestBuilder = new FairAPIRequestBuilder($fairAPI_url, $fairAPI_key, $fairAPI_secret);
-    }
-
-    public function checkBalance($email, $method, $amount){
-        return $this->sendRequest($this->requestBuilder->build(
-            'api/transaction',
-            array(
-                'email' => $email,
-                'method' => $method,
-                'amount' => $amount
-            )
-        ));
-    }
-
-    public function sendRequest(FairAPIRequest $request){
-
-        $functionUrl =
-            $request->getBaseUrl().'/'
-            .$request->getFunction();
-
-        if(count($request->getUrlParams())>0)
-            $finalUrl = $functionUrl.'?'.http_build_query($request->getUrlParams());
-        else $finalUrl = $functionUrl;
-
-        $transaction = new Signer(new ApiKey(
-            $request->getKey(),
-            $request->getSecret()
-        ));
-
-        $response = $transaction->transaction(
-            $finalUrl,
-            $request->getParams()
-        );
-
-        return $response;
-    }
-
-}
-
-class FairAPIRequestBuilder {
     private $fairAPI_url, $fairAPI_key, $fairAPI_secret;
+
     public function __construct($fairAPI_url, $fairAPI_key, $fairAPI_secret) {
         $this->fairAPI_url = $fairAPI_url;
         $this->fairAPI_key = $fairAPI_key;
         $this->fairAPI_secret = $fairAPI_secret;
     }
-    public function build($function, $params = array()){
-        return new FairAPIRequest(
-            $this->fairAPI_url,
-            $this->fairAPI_key,
-            $this->fairAPI_secret,
-            $function,
-            $params
+
+    public function checkBalance($email, $method, $amount){
+        $concept =  array(
+            'email' => $email,
+            'method' => $method,
+            'amount' => $amount
         );
-    }
-}
+        $functionUrl = $this->fairAPI_url.'/api/transaction';
 
-class FairAPIRequest extends ApiRequest {
-
-    private $fairAPI_key, $fairAPI_secret;
-
-    public function __construct($fairAPI_url, $fairAPI_key, $fairAPI_secret, $function, $urlParams) {
-        $this->fairAPI_key = $fairAPI_key;
-        $this->fairAPI_secret = $fairAPI_secret;
-        parent::__construct($fairAPI_url, $function, $urlParams, 'POST', array(), array());
-    }
-
-    public function getKey(){
-        return  $this->fairAPI_key;
-    }
-
-    public function getSecret(){
-        return  $this->fairAPI_secret;
-    }
-
-    public function getUrlParams(){
-        return array_merge(parent::getUrlParams(), array('apikey' => $this->fairAPI_key, 'nonce' => time()));
-    }
-
-    public function getHeaders(){
-        $stringToSign = $this->getBaseUrl() . '/' . $this->getFunction().'?'.http_build_query($this->getUrlParams());
-        $signature = hash_hmac('sha512', $stringToSign, $this->fairAPI_secret);
-        return array_merge(parent::getHeaders(), array(
-            'apisign' => $signature
+        $transaction = new Signer(new ApiKey(
+            $this->fairAPI_key,
+            $this->fairAPI_secret
         ));
+
+        $response = $transaction->transaction(
+            $functionUrl,
+            $concept
+        );
+        return $response;
     }
 }
 
