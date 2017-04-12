2ae5650a72c243c78b377e91147e1385f403f383
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2017-04-12 17:35:59 +0200

    sendRequest in Driver to do the debug tests

--- src/Telepay/FinancialApiBundle/Financial/Driver/FairAPIDriver.php
+++ src/Telepay/FinancialApiBundle/Financial/Driver/FairAPIDriver.php
@@ -16,7 +16,7 @@ class FairAPIDriver extends JsonRequester {
     }
 
     public function checkBalance($email, $method, $amount){
-        return $this->send($this->requestBuilder->build(
+        return $this->sendRequest($this->requestBuilder->build(
             'api/transaction',
             array(
                 'email' => $email,
@@ -25,6 +25,48 @@ class FairAPIDriver extends JsonRequester {
             )
         ));
     }
+
+    public function sendRequest(FairAPIRequest $request){
+
+        $functionUrl =
+            $request->getBaseUrl().'/'
+            .$request->getFunction();
+
+        if(count($request->getUrlParams())>0)
+            $finalUrl = $functionUrl.'?'.http_build_query($request->getUrlParams());
+        else $finalUrl = $functionUrl;
+
+
+        $ch = curl_init($finalUrl);
+
+        $headerArray = [];
+        foreach($request->getHeaders() as $key => $value){
+            $headerArray[]=ucfirst($key).': '.$value;
+        }
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headerArray);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $method = strtoupper($request->getMethod());
+        switch($method){
+            case "GET":
+                break;
+            case "POST":
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $request->getParams());
+                break;
+            case "DELETE":
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+                break;
+            case "PUT":
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($request->getParams()));
+                break;
+        }
+        return json_decode(curl_exec($ch));
+    }
+
 }
 
 class FairAPIRequestBuilder {
