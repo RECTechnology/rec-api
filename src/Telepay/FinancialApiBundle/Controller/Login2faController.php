b60f973d391fb51a3c8a3f129002f65078c3213c
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2017-05-25 17:41:22 +0200

    public login to signature GET data

--- src/Telepay/FinancialApiBundle/Controller/Login2faController.php
+++ src/Telepay/FinancialApiBundle/Controller/Login2faController.php
@@ -158,7 +158,7 @@ class Login2faController extends RestApiController{
         $data = array(
             'username' => $username,
             'access_secret' => substr($user[0]->getAccessSecret(), 0, 10),
-            'access_key' => substr($user[0]->getId() .  "A" . rand(0,1000) . "C"  . rand(0,1000) . "E" . rand(0,1000), 0, 10)
+            'access_key' => substr($user[0]->getId() .  "A" . rand(10,1000) . "C"  . rand(10,1000) . "E" . rand(10,1000), 0, 10)
         );
         return new Response(json_encode($data), 200, $headers);
     }
