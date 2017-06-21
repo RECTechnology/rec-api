a9ca3583ecb4ac8359465a1f994e20830e1a2672
Pere Andreu <pere@pasproduccions.com>
2017-06-21 18:20:17 +0200

    update fees from superadmin

--- src/Telepay/FinancialApiBundle/Controller/Management/Admin/CompanyFeesController.php
+++ src/Telepay/FinancialApiBundle/Controller/Management/Admin/CompanyFeesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Telepay\FinancialApiBundle\Controller\Management\Manager;
+namespace Telepay\FinancialApiBundle\Controller\Management\Admin;
 
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Telepay\FinancialApiBundle\Controller\BaseApiController;
@@ -10,9 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class GroupsController
- * @package Telepay\FinancialApiBundle\Controller\Manager
+ * @package Telepay\FinancialApiBundle\Controller\Admin
  */
-class CommissionsGroupController extends BaseApiController
+class CompanyFeesController extends BaseApiController
 {
     function getRepositoryName()
     {
@@ -27,23 +27,7 @@ class CommissionsGroupController extends BaseApiController
     /**
      * @Rest\View
      */
-    //TODO this maethod has to desappear-> only super admin can change fees
     public function updateAction(Request $request, $id){
-
-        $user = $this->get('security.context')->getToken()->getUser();
-
-        if(!$user->hasRole('ROLE_SUPER_ADMIN')){
-            //check if this user is the group crator or superadmin, if not access not allowed
-            $group = $user->getActiveGroup();
-            $em = $this->getDoctrine()->getManager();
-            $fee = $em->getRepository($this->getRepositoryName())->find($id);
-            $feeGroup = $fee->getGroup();
-
-            if($group->getId() != $feeGroup->getGroupCreator()->getId()) throw new HttpException(409, 'You don\'t have the necessary permissions to modify this group');
-
-            if(!$user->hasRole('ROLE_ADMIN')) throw new HttpException(403, 'You don\'t have the necessary permissions');
-        }
-
 
         //negative values not allowed in variable and fixed field
         if($request->request->has('fixed')){
