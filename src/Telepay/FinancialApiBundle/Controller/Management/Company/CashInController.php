b2113420f30ecf53807a95453fda2c0585f7515c
Pere Andreu <pere@pasproduccions.com>
2017-06-21 12:50:05 +0200

    solve problem with botc account

--- src/Telepay/FinancialApiBundle/Controller/Management/Company/CashInController.php
+++ src/Telepay/FinancialApiBundle/Controller/Management/Company/CashInController.php
@@ -66,7 +66,8 @@ class CashInController extends BaseApiController{
                         'beneficiary'   =>  ' XARXA INTEGRAL DE PROFESSIONALS I USUARIES',
                         'bic_swift' =>  'TRIOESMMXXX'
                     );
-                }else if($user->getActiveGroup()->getGroupCreator()->getId() == $this->container->getParameter('default_company_creator_commerce_botc')){
+                }
+                if($user->getActiveGroup()->getGroupCreator()->getId() == $this->container->getParameter('default_company_creator_commerce_botc')){
                     $info = array(
                         'iban'  =>  'ES43 1491 0001 2420 8685 5729',
                         'beneficiary'   =>  ' Xarxa Autogestió Social SCCL',
