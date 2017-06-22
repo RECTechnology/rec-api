e8c851b308c9aaa2d460a021e12130df26e6c58e
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2017-06-22 13:35:23 +0200

    update tier for botc admin users reseller

--- src/Telepay/FinancialApiBundle/Controller/Management/Company/SpecialActionsController.php
+++ src/Telepay/FinancialApiBundle/Controller/Management/Company/SpecialActionsController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Telepay\FinancialApiBundle\Controller\RestApiController;
 use Telepay\FinancialApiBundle\Document\Transaction;
+use Telepay\FinancialApiBundle\Entity\Group;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use WebSocket\Exception;
@@ -409,5 +410,25 @@ class SpecialActionsController extends RestApiController {
 
         return $total;
 
+    }
+
+    public function changeTier(Request $request, $company_id, $tier){
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_WORKER')) {
+            throw $this->createAccessDeniedException();
+        }
+        //Function only available for botc
+        $botc_id = $this->container->getParameter('default_company_creator_commerce_botc');
+        $user = $this->get('security.context')->getToken()->getUser();
+        $activeCompany = $user->getActiveGroup();
+        if($activeCompany->getId() != $botc_id) throw new HttpException(403, 'You don\'t have the necessary permissions');
+        $em = $this->getDoctrine()->getManager();
+        $group = $em->getRepository('TelepayFinancialApiBundle:Group')->findOneBy(array(
+            'id'  =>  $company_id,
+            'group_creator_id' =>  $botc_id
+        ));
+        if(!$group) throw new HttpException(404, 'Group not allowed');
+        $group->setTier($tier);
+        $em->flush();
+        return $this->rest(204, 'Company tier updated successfully');
     }
 }
