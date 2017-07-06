9e7bc86ef34a25116b528301f796ec33640df315
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2017-07-06 20:53:43 +0200

    all currency in ticker list

--- src/Telepay/FinancialApiBundle/Financial/Currency.php
+++ src/Telepay/FinancialApiBundle/Financial/Currency.php
@@ -18,8 +18,8 @@ class Currency {
     public static $PLN = "PLN";
     public static $LISTA = array("BTC","EUR","USD","FAC","MXN","PLN");
     public static $ALL = array("BTC","EUR","USD","FAC","MXN","PLN");
-    //public static $TICKER_LIST = array("BTC","EUR","USD","FAC","MXN","PLN","FAIRP","ETH","CREA");
-    public static $TICKER_LIST = array("BTC","EUR","USD","FAC","MXN","PLN","FAIRP");
+    public static $TICKER_LIST = array("BTC","EUR","USD","FAC","MXN","PLN","FAIRP","ETH","CREA");
+    //public static $TICKER_LIST = array("BTC","EUR","USD","FAC","MXN","PLN","FAIRP");
     public static $TICKER_FAIRCOOP = array("BTC","EUR","USD","FAIRP","MXN","PLN");
     public static $SCALE = array(
         "BTC" => 8,
