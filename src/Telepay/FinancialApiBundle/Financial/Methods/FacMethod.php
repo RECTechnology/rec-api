527ce115977d20f14e2b12c778bc4c8e117fe036
Pere Andreu <pere@pasproduccions.com>
2017-09-28 14:05:09 +0200

    Launch exception when no funds in fair wallet

--- src/Telepay/FinancialApiBundle/Financial/Methods/FacMethod.php
+++ src/Telepay/FinancialApiBundle/Financial/Methods/FacMethod.php
@@ -141,6 +141,9 @@ class FacMethod extends  BaseMethod {
 
         if(!$address_verification['isvalid']) throw new Exception('Invalid address.', 400);
 
+        if($this->driver->getbalance() <= $params['amount'] / 1e8) throw new HttpException(403, 'Service Temporally unavailable');
+
+
         if(array_key_exists('concept', $data)){
             $params['concept'] = $data['concept'];
         }else{
