5fe6610375bd0d9d24e11a0e2445624ec8a28631
Pere Andreu <pere@pasproduccions.com>
2017-10-03 14:06:03 +0200

    solve problem with sendEmail Sepa out

--- src/Telepay/FinancialApiBundle/Financial/Methods/SepaMethod.php
+++ src/Telepay/FinancialApiBundle/Financial/Methods/SepaMethod.php
@@ -9,13 +9,13 @@
 namespace Telepay\FinancialApiBundle\Financial\Methods;
 
 use FOS\OAuthServerBundle\Util\Random;
-use MongoDBODMProxies\__CG__\Telepay\FinancialApiBundle\Document\Transaction;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Telepay\FinancialApiBundle\DependencyInjection\Transactions\Core\BaseMethod;
 use Telepay\FinancialApiBundle\DependencyInjection\Transactions\Core\CashInInterface;
 use Telepay\FinancialApiBundle\DependencyInjection\Transactions\Core\CashOutInterface;
+use Telepay\FinancialApiBundle\Document\Transaction;
 use Telepay\FinancialApiBundle\Financial\Currency;
 
 class SepaMethod extends BaseMethod {
