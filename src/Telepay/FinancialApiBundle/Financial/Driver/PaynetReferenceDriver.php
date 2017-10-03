cd53fd06de12dc6c2e626c78f5cdae3261932540
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2017-10-03 10:47:12 +0200

    send ether with big amounts working properly

--- src/Telepay/FinancialApiBundle/Financial/Driver/PaynetReferenceDriver.php
+++ src/Telepay/FinancialApiBundle/Financial/Driver/PaynetReferenceDriver.php
@@ -33,11 +33,6 @@ class PaynetReferenceDriver{
             'description' 		=> 	$this->description          //'television'
         );
 
-        //URL de Test
-        //$client = new nusoap_client('http://201.147.99.51/PaynetCE/WSPaynetCE.asmx?WSDL', true);
-        //URL de prod
-        //$client = new nusoap_client('https://www.datalogic.com.mx/PaynetCE/WSPaynetCE.asmx?WSDL', true);
-
         $client = new nusoap_client($this->url, true);
 
         $result = $client -> call('GetPaynetReference',$params);
