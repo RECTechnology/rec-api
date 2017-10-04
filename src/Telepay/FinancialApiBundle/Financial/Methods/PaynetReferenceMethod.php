3589ea92c6143b15e87cfcc20d034146bd0456b6
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2017-10-05 01:10:31 +0200

    result data

--- src/Telepay/FinancialApiBundle/Financial/Methods/PaynetReferenceMethod.php
+++ src/Telepay/FinancialApiBundle/Financial/Methods/PaynetReferenceMethod.php
@@ -68,7 +68,7 @@ class PaynetReferenceMethod extends BaseMethod{
         if(isset($paymentInfo['paynet_id'])){
             $client_reference = $paymentInfo['paynet_id'];
             $result = $this->driver->status($client_reference);
-            if(isset($paymentInfo['error_code'])){
+            if(isset($result['error_code'])){
                 $paymentInfo['status'] = 'error';
                 $paymentInfo['paynet_status'] = 'Not paynet id found';
                 $paymentInfo['final'] = true;
