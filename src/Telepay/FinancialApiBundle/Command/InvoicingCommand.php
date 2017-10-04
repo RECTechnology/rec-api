6bf6f0d144a25016e42a546dd09b9d8837377ae4
Pere Andreu <pere@pasproduccions.com>
2017-10-04 18:19:49 +0200

    implementation invoicing command

---
+++ src/Telepay/FinancialApiBundle/Command/InvoicingCommand.php
@@ -0,0 +1,133 @@
+<?php
+namespace Telepay\FinancialApiBundle\Command;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Telepay\FinancialApiBundle\Document\Transaction;
+
+class InvoicingCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('telepay:invoice:generator')
+            ->setDescription('Generate invoices by month')
+            ->addOption(
+                'start_date',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Define the start date',
+                null
+            )
+            ->addOption(
+                'finish_date',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Define the finish date',
+                null
+            )
+        ;
+    }
+
+    public $start_date;
+    public $finish_date;
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+
+        $this->start_date = new \MongoDate(strtotime($input->getOption('start_date') .' 00:00:00'));
+        $this->finish_date = new \MongoDate(strtotime($input->getOption('finish_date') .' 00:00:00'));
+
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $dm = $this->getContainer()->get('doctrine_mongodb')->getManager();
+
+        $companies = $em->getRepository('TelepayFinancialApiBundle:Group')->findAll();
+
+        $qb = $dm->createQueryBuilder('TelepayFinancialApiBundle:Transaction');
+
+        $resume = array();
+        foreach($companies as $company){
+            if($company->getName() != 'root'){
+                if($company->hasRole('ROLE_COMPANY')){
+                    //search all success fees transactions by group
+//                die(print_r($company->getName(),true));
+                    $result = $qb
+                        ->field('updated')->gte($this->start_date)
+                        ->field('updated')->lte($this->finish_date)
+                        ->field('status')->equals(Transaction::$STATUS_SUCCESS)
+                        ->field('group')->equals($company->getId())
+                        ->field('type')->equals(Transaction::$TYPE_FEE)
+                        ->getQuery()
+                        ->execute();
+
+                    if(count($result) > 1){
+                        $fees = array();
+                        foreach($result->toArray() as $transaction){
+                            $fixed = $transaction->getFixedFee();
+                            $variable = $transaction->getVariableFee();
+
+                            $currency = $transaction->getCurrency();
+
+                            $feeInfo = $transaction->getFeeInfo();
+
+                            //TODO calculate % variable fee
+                            $variableFee = 100*$transaction->getAmount()/$feeInfo['previous_amount'];
+
+                            //TODO exchange
+                            if($currency!='EUR'){
+                                die(print_r($transaction,true));
+                            }
+
+                            if(isset($fees[$transaction->getMethod()])){
+                                // check if fixed and variable are the same
+                                $exist = 0;
+                                foreach ($fees[$transaction->getMethod()] as $feeInfo){
+                                    if($feeInfo['fixed'] == $fixed && $feeInfo['variable'] == $variableFee){
+                                        //add information
+                                        $feeInfo['counter'] = $feeInfo['counter'] +1;
+                                        $feeInfo['total']   = $feeInfo['total'] + $transaction->getAmount();
+                                        $exist = 1;
+                                    }
+                                }
+
+                                if($exist == 0){
+                                    $information = array(
+                                        'fixed' =>  $fixed,
+                                        'variable' =>   $variableFee,
+                                        'counter'   =>  1,
+                                        'total' =>  $transaction->getAmount()
+                                    );
+                                    $fees[$transaction->getMethod()][] = $information;
+                                }
+
+
+
+                            }else{
+                                $information = array(
+                                    'fixed' =>  $fixed,
+                                    'variable' =>   $variableFee,
+                                    'counter'   =>  1,
+                                    'total' =>  $transaction->getAmount()
+                                );
+                                $fees[$transaction->getMethod()][] = $information;
+
+                            }
+//                            die(print_r($fees,true));
+
+                        }
+
+                        $resume[$company->getName()] = $fees;
+
+                        die(print_r($resume,true));
+
+                    }
+                }
+
+            }
+        }
+
+    }
+}
+
