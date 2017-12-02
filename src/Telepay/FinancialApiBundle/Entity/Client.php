b3af87441e30a11228f1ca3e590c213493607c8b
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2017-12-02 19:19:00 +0100

    swift data deleted from client

--- src/Telepay/FinancialApiBundle/Entity/Client.php
+++ src/Telepay/FinancialApiBundle/Entity/Client.php
@@ -34,26 +34,9 @@ class Client extends BaseClient
     private $group;
 
     /**
-     * @ORM\Column(type="string", length=1000)
-     */
-    private $swift_list;
-
-    /**
      * @Expose
      */
     private $group_data = array();
-
-    /**
-     * @ORM\OneToMany(targetEntity="Telepay\FinancialApiBundle\Entity\SwiftLimit", mappedBy="client", cascade={"remove"})
-     *
-     */
-    private $swif_limits;
-
-    /**
-     * @ORM\OneToMany(targetEntity="Telepay\FinancialApiBundle\Entity\SwiftFee", mappedBy="client", cascade={"remove"})
-     *
-     */
-    private $swift_fees;
 
     /**
      * @ORM\Column(type="string", nullable=true)
@@ -80,107 +63,6 @@ class Client extends BaseClient
     public function setName($name)
     {
         $this->name = $name;
-    }
-
-//    /**
-//     * @return mixed
-//     */
-//    public function getUser()
-//    {
-//        return $this->user;
-//    }
-//
-//    /**
-//     * @param mixed $user
-//     */
-//    public function setUser($user)
-//    {
-//        $this->user = $user;
-//    }
-
-    /**
-     * @return mixed
-     */
-    public function getSwiftList()
-    {
-        return json_decode($this->swift_list);
-    }
-
-    /**
-     * @param mixed $swift_list
-     */
-    public function setSwiftList($swift_list)
-    {
-
-        $actual_list = $this->getSwiftList();
-        $new_list = array();
-        $add_list = array();
-        if($actual_list != ''){
-            foreach ($swift_list as $swift){
-                $matriz = preg_grep('/'.$swift.'/i',$actual_list);
-
-                if(count($matriz) >= 1){
-                    $new_list[] = implode('',$matriz);
-                }else{
-                    $add_list[] = $swift;
-                }
-
-            }
-            foreach($add_list as $add){
-                $new_list[] = $add.':0';
-            }
-
-        }else{
-            foreach($swift_list as $swift){
-                $new_list[] = $swift.':0';
-            }
-        }
-
-        $this->swift_list = json_encode($new_list);
-    }
-
-    /**
-     * @param mixed $swift_list
-     */
-    public function activeSwiftList($swift_list)
-    {
-        $actual_list = $this->getSwiftList();
-
-        for($i = 0; $i<count($actual_list); $i++){
-            $params = explode(':',$actual_list[$i]);
-            $actual_list[$i] = $params[0].':0';
-        }
-
-        if($swift_list != 'disable_all'){
-            for($j = 0; $j<count($actual_list); $j++){
-                foreach($swift_list as $swift){
-                    if(preg_match('/'.$swift.'/i',$actual_list[$j])){
-                        $actual_list[$j] = $swift.':1';
-                    }
-                }
-            }
-        }
-
-
-        $this->swift_list = json_encode($actual_list);
-    }
-
-    /**
-     * @param mixed $cname
-     */
-    public function addService($cname){
-        $new = array($cname);
-        $merge = array_merge($this->swift_list, $new);
-        $result = array_unique($merge, SORT_REGULAR);
-        $this->swift_list = json_encode($result);
-    }
-
-    /**
-     * @param mixed $cname
-     */
-    public function removeService($cname){
-        $result = array_diff(json_decode($this->swift_list), array($cname));
-        $this->swift_list = json_encode(array_values($result));
     }
 
     /**
@@ -213,38 +95,6 @@ class Client extends BaseClient
     public function setGroupData($group_data)
     {
         $this->group_data = $group_data;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSwifLimits()
-    {
-        return $this->swif_limits;
-    }
-
-    /**
-     * @param mixed $swif_limits
-     */
-    public function setSwifLimits($swif_limits)
-    {
-        $this->swif_limits = $swif_limits;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSwiftFees()
-    {
-        return $this->swift_fees;
-    }
-
-    /**
-     * @param mixed $swift_fees
-     */
-    public function setSwiftFees($swift_fees)
-    {
-        $this->swift_fees = $swift_fees;
     }
 
     /**
