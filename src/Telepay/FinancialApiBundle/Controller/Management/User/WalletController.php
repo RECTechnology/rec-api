25162cb567468361377797227e8e4a72964b79e6
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2018-07-30 17:43:44 +0200

    only internal = false transaction in the main list

--- src/Telepay/FinancialApiBundle/Controller/Management/User/WalletController.php
+++ src/Telepay/FinancialApiBundle/Controller/Management/User/WalletController.php
@@ -291,7 +291,7 @@ class WalletController extends RestApiController{
         if($request->query->get('query')){
             $query = $request->query->get('query');
             $qb->field('group')->equals($userGroup->getId());
-            $qb->field('internal')->notIn(array(true));
+            $qb->field('internal')->equals(false);
 
             if(isset($query['start_date'])){
                 $start_time = new \MongoDate(strtotime(date($query['start_date'].' 00:00:00')));
