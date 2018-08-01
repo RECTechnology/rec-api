ee00d20cabfbe183faeba3a3d0cf70d34f70f4d2
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2018-08-01 14:00:39 +0200

    use CreditCard entity

--- src/Telepay/FinancialApiBundle/Controller/Transactions/NotificationsController.php
+++ src/Telepay/FinancialApiBundle/Controller/Transactions/NotificationsController.php
@@ -8,6 +8,8 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Telepay\FinancialApiBundle\Controller\RestApiController;
+use Telepay\FinancialApiBundle\Entity\CreditCard;
+
 
 class NotificationsController extends RestApiController{
 
