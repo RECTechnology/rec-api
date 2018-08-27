225f7569fec71502d62b39b0c2e3b2701bc3b71f
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2018-08-27 18:54:20 +0200

    csv directory defined in parameters

--- src/Telepay/FinancialApiBundle/Command/DelegatedExchangeCommand.php
+++ src/Telepay/FinancialApiBundle/Command/DelegatedExchangeCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
@@ -25,9 +26,8 @@ class DelegatedExchangeCommand extends ContainerAwareCommand
     }
 
     private $cvsParsingOptions = array(
-        'finder_in' => "~",
         'finder_name' => 'delegated_changes.csv',
-        'ignoreFirstRow' => true
+        'ignoreFirstRow' => false
     );
 
     protected function execute(InputInterface $input, OutputInterface $output){
@@ -39,7 +39,7 @@ class DelegatedExchangeCommand extends ContainerAwareCommand
         foreach ($csv as $line) {
             $dni_user = $line[0];
             $cif_commerce = $line[1];
-            $amount = $line[2]*100;
+            $amount = intval($line[2])*100;
             /*
                         $request = array();
                         $request['concept'] = 'Internal exchange';
@@ -63,7 +63,7 @@ class DelegatedExchangeCommand extends ContainerAwareCommand
         $ignoreFirstRow = $this->cvsParsingOptions['ignoreFirstRow'];
         $finder = new Finder();
         $finder->files()
-            ->in($this->cvsParsingOptions['finder_in'])
+            ->in($this->getContainer()->getParameter('csv_dir'))
             ->name($this->cvsParsingOptions['finder_name'])
             ->files();
         foreach ($finder as $file) {
