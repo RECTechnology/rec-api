c818979329441a22241d58a23c946e30ebd49b7f
Ivan Barrachina Bellmunt <ivanvdc89@gmail.com>
2018-11-15 15:23:58 +0100

    check balance with firts id parameter

--- src/Telepay/FinancialApiBundle/Command/CheckBalanceCommand.php
+++ src/Telepay/FinancialApiBundle/Command/CheckBalanceCommand.php
@@ -20,29 +20,44 @@ class CheckBalanceCommand extends ContainerAwareCommand{
         $this
             ->setName('rec:check:balance')
             ->setDescription('Check rec balances')
+            ->addOption(
+                'first_id',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Define the fisrt group id',
+                null
+            )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output){
+        $first = $input->getOption('first_id');
+        $first = intval($first);
+
         $em = $this->getContainer()->get('doctrine')->getManager();
         $groupList = $em->getRepository('TelepayFinancialApiBundle:Group')->findAll();
         $scale = 100000000;
 
         foreach ( $groupList as $account ){
-            $wallet = $account->getWallet('rec');
-            $address = $account->getRecAddress();
-            $cryptoProvider = $this->getContainer()->get('net.telepay.in.rec.v1');
-            $rec_balance_0 = $cryptoProvider->getReceivedByAddress($address,0);
-            $wallet->setBlockchainPending($rec_balance_0*$scale);
-            $rec_balance_1 = $cryptoProvider->getReceivedByAddress($address,1);
-            $wallet->setBlockchain($rec_balance_1*$scale);
-            $output->writeln($account->getId() . ': ' . $wallet->getBalance() . " " . $rec_balance_0*$scale . " " . $rec_balance_1*$scale);
-            if($wallet->getBalance() != $rec_balance_1*$scale){
-                exec('curl -X POST -d "chat_id=-250635592&text=#ERROR BALANCE ' . $account->getId() . '" ' . '"https://api.telegram.org/bot787861588:AAFWCYdIiAoltb0IoM71jlmzq3AHh8kXSMs/sendMessage"');
+            if($account->getId() >= $first){
+                $wallet = $account->getWallet('rec');
+                $address = $account->getRecAddress();
+                $cryptoProvider = $this->getContainer()->get('net.telepay.in.rec.v1');
+                $rec_balance = $cryptoProvider->getReceivedByAddress($address,0);
+                $rec_balance_0 = intval($rec_balance*$scale);
+                $wallet->setBlockchainPending($rec_balance_0);
+                $balance = $wallet->getBalance();
+                $rec_balance = $cryptoProvider->getReceivedByAddress($address,1);
+                $rec_balance_1 = intval($rec_balance*$scale);
+                $wallet->setBlockchain($rec_balance_1);
+                $output->writeln($account->getId() . ': ' . $balance . " " . $rec_balance_0 . " " . $rec_balance_1);
+                if(intval($balance) != intval($rec_balance_1)){
+                    exec('curl -X POST -d "chat_id=-250635592&text=#ERROR BALANCE ' . $account->getId() . "=>" . $balance . "!=" . $rec_balance_1 . '" ' . '"https://api.telegram.org/bot787861588:AAFWCYdIiAoltb0IoM71jlmzq3AHh8kXSMs/sendMessage"');
+                }
+                $em->persist($wallet);
+                $em->flush();
+                sleep(20);
             }
-            $em->persist($wallet);
-            $em->flush();
-            sleep(20);
         }
     }
 }
