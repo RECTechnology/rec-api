e230b725eb13d9ef46b4a9223336844e0b595f9d
Lluis Santos <lluis@ngynx.com>
2019-05-07 13:25:30 +0200

    checked for not null values before validating expiry_date

--- src/Telepay/FinancialApiBundle/Validator/Constraint/IsNotExpiredValidator.php
+++ src/Telepay/FinancialApiBundle/Validator/Constraint/IsNotExpiredValidator.php
@@ -26,19 +26,21 @@ class IsNotExpiredValidator extends ConstraintValidator {
      */
     public function validate($value, Constraint $constraint)
     {
-        $expiry_date = explode("/", $value);
-        if(count($expiry_date) !== 2)
-            $expiry_date = explode("-", $value);
+        if($value !== null){
+            $expiry_date = explode("/", $value);
+            if(count($expiry_date) !== 2)
+                $expiry_date = explode("-", $value);
 
-        assert(count($expiry_date) === 2);
+            assert(count($expiry_date) === 2);
 
-        $expiry_month = intval($expiry_date[0]);
-        $expiry_year = intval(strlen($expiry_date[1]) == 2? "20" . $expiry_date[1]: $expiry_date[1]);
-        if($expiry_year > intval(date('Y'))) return;
-        if($expiry_year < intval(date('Y')) or $expiry_month < intval(date('m'))) {
-            $this->context->addViolation(
-                $constraint->message
-            );
+            $expiry_month = intval($expiry_date[0]);
+            $expiry_year = intval(strlen($expiry_date[1]) == 2? "20" . $expiry_date[1]: $expiry_date[1]);
+            if($expiry_year > intval(date('Y'))) return;
+            if($expiry_year < intval(date('Y')) or $expiry_month < intval(date('m'))) {
+                $this->context->addViolation(
+                    $constraint->message
+                );
+            }
         }
     }
 }
