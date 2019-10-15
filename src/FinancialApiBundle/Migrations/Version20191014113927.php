5ea585a7344eb95bc0b39ec10f44f4379b7ff4fb
Lluis Santos <lluis@ngynx.com>
2019-10-15 18:21:04 +0200

    separated migration in 2 steps: schema migration and data migration

--- src/FinancialApiBundle/Migrations/Version20191014113927.php
+++ src/FinancialApiBundle/Migrations/Version20191014113927.php
@@ -13,33 +13,46 @@ use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 /**
- * Class Version20191014113926
+ * Class Version20191014113927
  * @package App\FinancialApiBundle\Migrations
  */
-final class Version20191014113926 extends AbstractMigration implements ContainerAwareInterface{
+final class Version20191014113927 extends AbstractMigration implements ContainerAwareInterface{
 
     use ContainerAwareTrait;
 
     public function getDescription() : string
     {
-        return 'Adds status to activities and products (only schema)';
+        return 'Sets status default value (only data)';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('ALTER TABLE Activity ADD status VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE ProductKind ADD status VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
 
-        $this->addSql('ALTER TABLE Activity DROP status');
-        $this->addSql('ALTER TABLE ProductKind DROP status');
+
+    public function postUp(Schema $schema): void {
+        parent::postUp($schema);
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $repo = $em->getRepository(Activity::class);
+        /** @var Activity $entity */
+        foreach ($repo->findAll() as $entity){
+            $entity->setStatus(Activity::STATUS_CREATED);
+            $em->persist($entity);
+        }
+        $repo = $em->getRepository(ProductKind::class);
+        /** @var ProductKind $activity */
+        foreach ($repo->findAll() as $entity){
+            $entity->setStatus(Activity::STATUS_CREATED);
+            $em->persist($entity);
+        }
+        $em->flush();
     }
 }
