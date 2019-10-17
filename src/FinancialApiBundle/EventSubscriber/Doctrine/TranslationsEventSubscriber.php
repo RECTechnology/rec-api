1bbdc8332753773bb2a690964bc0d39228aad077
Lluis Santos <lluis@ngynx.com>
2019-10-17 17:47:04 +0200

    fixes bug with commands (translations wont be available in commands)

--- src/FinancialApiBundle/EventSubscriber/Doctrine/TranslationsEventSubscriber.php
+++ src/FinancialApiBundle/EventSubscriber/Doctrine/TranslationsEventSubscriber.php
@@ -12,6 +12,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
@@ -79,24 +80,26 @@ class TranslationsEventSubscriber implements EventSubscriber {
      */
     function saveTranslations($entity, $changeSet = []){
         if($entity instanceof Translatable){
-            $locale = $this->stack->getCurrentRequest()->getLocale();
-            $defaultLocale = $this->stack->getCurrentRequest()->getDefaultLocale();
-            if($locale != $defaultLocale){
-                $rc = new \ReflectionClass($entity);
-                foreach($rc->getProperties() as $rp){
-                    if(array_key_exists($rp->name, $changeSet)){
-                        $ar = new AnnotationReader();
-                        foreach ($ar->getPropertyAnnotations($rp) as $an){
-                            if($an instanceof TranslatedProperty){
-                                $rp->setAccessible(true);
-                                $translatedFieldName = implode("_", [$rp->name, $locale]);
-                                if(!$rc->hasProperty($translatedFieldName))
-                                    throw new NoSuchTranslationException("This object cannot be translated to " . $locale);
-                                $translationProperty = $rc->getProperty($translatedFieldName);
-                                $translationProperty->setAccessible(true);
-                                $translationProperty->setValue($entity, $rp->getValue($entity));
-                                if(count($changeSet) <= 0) $rp->setValue($entity, null);
-                                else $rp->setValue($entity, $changeSet[$rp->name][0]);
+            if($this->stack->getCurrentRequest()){
+                $locale = $this->stack->getCurrentRequest()->getLocale();
+                $defaultLocale = $this->stack->getCurrentRequest()->getDefaultLocale();
+                if($locale != $defaultLocale){
+                    $rc = new \ReflectionClass($entity);
+                    foreach($rc->getProperties() as $rp){
+                        if(array_key_exists($rp->name, $changeSet)){
+                            $ar = new AnnotationReader();
+                            foreach ($ar->getPropertyAnnotations($rp) as $an){
+                                if($an instanceof TranslatedProperty){
+                                    $rp->setAccessible(true);
+                                    $translatedFieldName = implode("_", [$rp->name, $locale]);
+                                    if(!$rc->hasProperty($translatedFieldName))
+                                        throw new NoSuchTranslationException("This object cannot be translated to " . $locale);
+                                    $translationProperty = $rc->getProperty($translatedFieldName);
+                                    $translationProperty->setAccessible(true);
+                                    $translationProperty->setValue($entity, $rp->getValue($entity));
+                                    if(count($changeSet) <= 0) $rp->setValue($entity, null);
+                                    else $rp->setValue($entity, $changeSet[$rp->name][0]);
+                                }
                             }
                         }
                     }
@@ -114,21 +117,23 @@ class TranslationsEventSubscriber implements EventSubscriber {
     public function postLoad(LifecycleEventArgs $args){
         $entity = $args->getEntity();
         if($entity instanceof Translatable){
-            $locale = $this->stack->getCurrentRequest()->getLocale();
-            $defaultLocale = $this->stack->getCurrentRequest()->getDefaultLocale();
-            if($locale != $defaultLocale){
-                $rc = new \ReflectionClass($entity);
-                foreach($rc->getProperties() as $rp){
-                    $ar = new AnnotationReader();
-                    foreach ($ar->getPropertyAnnotations($rp) as $an){
-                        if($an instanceof TranslatedProperty){
-                            $rp->setAccessible(true);
-                            $translatedFieldName = implode("_", [$rp->name, $locale]);
-                            if($rc->hasProperty($translatedFieldName)){
-                                $translationProperty = $rc->getProperty($translatedFieldName);
-                                $translationProperty->setAccessible(true);
-                                $value = $translationProperty->getValue($entity);
-                                if($value) $rp->setValue($entity, $value);
+            if($this->stack->getCurrentRequest()){
+                $locale = $this->stack->getCurrentRequest()->getLocale();
+                $defaultLocale = $this->stack->getCurrentRequest()->getDefaultLocale();
+                if($locale != $defaultLocale){
+                    $rc = new \ReflectionClass($entity);
+                    foreach($rc->getProperties() as $rp){
+                        $ar = new AnnotationReader();
+                        foreach ($ar->getPropertyAnnotations($rp) as $an){
+                            if($an instanceof TranslatedProperty){
+                                $rp->setAccessible(true);
+                                $translatedFieldName = implode("_", [$rp->name, $locale]);
+                                if($rc->hasProperty($translatedFieldName)){
+                                    $translationProperty = $rc->getProperty($translatedFieldName);
+                                    $translationProperty->setAccessible(true);
+                                    $value = $translationProperty->getValue($entity);
+                                    if($value) $rp->setValue($entity, $value);
+                                }
                             }
                         }
                     }
