797cea4be7c0be658dd7cd947c44f7b2c9df92e2
Lluis Santos <lluis@qbitartifacts.com>
2019-11-19 13:55:20 +0100

    feat(migrations): adding migration to support lemonway balance in accounts

---
+++ src/FinancialApiBundle/Migrations/Version20191119125332.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FinancialApiBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Class Version20191119125332
+ * @package App\FinancialApiBundle\Migrations
+ */
+final class Version20191119125332 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Adds lemonway balance to account table';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE fos_group ADD lw_balance INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE fos_group DROP lw_balance');
+    }
+}
