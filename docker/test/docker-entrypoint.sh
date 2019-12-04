087def17e87ab877202a1fd18ff1ef5b5c3be7f1
Lluis Santos <lluis@qbitartifacts.com>
2019-12-04 15:36:32 +0100

    improved coverage test

--- docker/test/docker-entrypoint.sh
+++ docker/test/docker-entrypoint.sh
@@ -10,7 +10,7 @@ case $1 in
   coverage)
     composer install --no-interaction
     bin/console --env=test cache:clear
-    vendor/bin/phpunit --coverage-text
+    vendor/bin/phpunit --coverage-text | grep -B 3 "^Statistics:" | grep Lines | awk '{print $2}'
     ;;
   *)
     exec "$@"
