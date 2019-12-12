4878bc1bd85f4e150fc1b40d7fae186567046ad1
Lluis Santos <lluis@ngynx.com>
2019-12-12 16:34:40 +0100

    fix(ci): fixed coverage tests (#162)
    * fix(ci): fixed coverage tests
    
    * chore(release): 2.2.0

--- docker/test/docker-entrypoint.sh
+++ docker/test/docker-entrypoint.sh
@@ -8,8 +8,8 @@ case $1 in
     vendor/bin/phpunit
     ;;
   coverage)
-    composer install --no-interaction
-    bin/console --env=test cache:clear
+    composer install --no-interaction > /dev/null 2>&1
+    bin/console --env=test cache:clear > /dev/null 2>&1
     vendor/bin/phpunit --coverage-text 2> /dev/null | grep -A 3 "Summary:" | grep "Lines" | awk '{print $3}' | sed -E 's/\.[0-9]*%//g'
     ;;
   *)
