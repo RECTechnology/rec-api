d9334e70ece0ff3defc415eaea880ac3e023f5cc
Lluis Santos <lluis@qbitartifacts.com>
2019-12-17 13:14:19 +0100

    feat(migrations): added migration to persist lemon document status

---
+++ src/FinancialApiBundle/Migrations/Version20191217120918.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FinancialApiBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Class Version20191217120918
+ * @package App\FinancialApiBundle\Migrations
+ */
+final class Version20191217120918 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Adds lemon_status to lemonway documents';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Document ADD lemon_status INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Document DROP lemon_status');
+    }
+}
