1a5d13e50ca71aeb3a8bc01a20fbcd4c735d6f4b
Lluis Santos <lluis@qbitartifacts.com>
2020-01-31 14:06:25 +0100

    temporal commit just to push the changes

--- src/FinancialApiBundle/Controller/Management/Company/CreditCardController.php
+++ src/FinancialApiBundle/Controller/Management/Company/CreditCardController.php
@@ -57,7 +57,6 @@ class CreditCardController extends BaseApiController{
     }
 
 
-
     /**
      * @Rest\View
      */
@@ -65,11 +64,8 @@ class CreditCardController extends BaseApiController{
         $user = $this->getUser();
         $company = $user->getActiveGroup();
         $em = $this->getDoctrine()->getManager();
-        $cards = $em->getRepository('FinancialApiBundle:CreditCard')->findBy(array(
-            'company'   =>  $company,
-            'deleted'=>false,
-            'user'   =>  $user
-        ));
+        $cards = $em->getRepository(CreditCard::class)
+            ->findBy(['company'   =>  $company, 'deleted'=>false, 'user' => $user]);
         return $this->restV2(200, 'ok', 'Request successfull', $cards);
     }
 
