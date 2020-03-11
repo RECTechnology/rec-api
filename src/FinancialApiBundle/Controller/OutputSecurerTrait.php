662542bf67588ed4472d87a1d977ac9aae0992c3
Lluis Santos <lluis@qbitartifacts.com>
2020-03-11 19:05:39 +0100

    fixed behaviour for classes which dont implements getSerializationContext

--- src/FinancialApiBundle/Controller/OutputSecurerTrait.php
+++ src/FinancialApiBundle/Controller/OutputSecurerTrait.php
@@ -14,45 +14,14 @@ trait OutputSecurerTrait
 {
 
     /**
-     * @return SerializationContext
-     */
-    private function getSerializationContext() {
-
-        /** @var TokenStorageInterface $tokenStorage */
-        $tokenStorage = $this->get('security.token_storage');
-
-        /** @var AuthorizationCheckerInterface $auth */
-        $auth = $this->get('security.authorization_checker');
-
-        $grantsMap = [
-            self::ROLE_ROOT => Group::SERIALIZATION_GROUPS_ROOT,
-            self::ROLE_SUPER_ADMIN => Group::SERIALIZATION_GROUPS_ADMIN,
-            self::ROLE_SUPER_MANAGER => Group::SERIALIZATION_GROUPS_MANAGER,
-            self::ROLE_SUPER_USER => Group::SERIALIZATION_GROUPS_USER,
-            'IS_AUTHENTICATED_ANONYMOUSLY' => Group::SERIALIZATION_GROUPS_PUBLIC,
-        ];
-
-        $ctx = new SerializationContext();
-        $ctx->enableMaxDepthChecks();
-        if($tokenStorage->getToken()){
-            foreach($grantsMap as $grant => $serializationGroup){
-                if($auth->isGranted($grant)) {
-                    $ctx->setGroups($serializationGroup);
-                    return $ctx;
-                }
-            }
-        }
-
-        $ctx->setGroups(Group::SERIALIZATION_GROUPS_PUBLIC);
-        return $ctx;
-    }
-
-    /**
      * @param $result
      * @return array|null
      */
     protected function secureOutput($result) {
-        $ctx = $this->getSerializationContext();
+        if (method_exists($this, 'getSerializationContext'))
+            $ctx = $this->getSerializationContext();
+        else
+            $ctx = new SerializationContext();
         /** @var Serializer $serializer */
         $serializer = $this->get('jms_serializer');
         return $serializer->toArray($result, $ctx);
