09480edeb86c89b7be334e6acf48defc7c0dce47
Lluis Santos <lluis@qbitartifacts.com>
2020-03-18 18:34:40 +0100

    fix(migrations): added migration to set all accounts to country=ESP, fixes #237

---
+++ src/FinancialApiBundle/Migrations/Version20200318171535.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FinancialApiBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Class Version20200318171535
+ * @package App\FinancialApiBundle\Migrations
+ */
+final class Version20200318171535 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return "Sets all Account's country to ESP";
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("UPDATE fos_group SET country='ESP'");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
