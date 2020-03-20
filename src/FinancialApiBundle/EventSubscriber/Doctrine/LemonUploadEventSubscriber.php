9d7a3fc0257cabb045aa0340bf81fef918a2d074
Lluis Santos <lluis@qbitartifacts.com>
2020-03-20 15:22:03 +0100

    fix(lemonway): fixed bug in lemon uploader

--- src/FinancialApiBundle/EventSubscriber/Doctrine/LemonUploadEventSubscriber.php
+++ src/FinancialApiBundle/EventSubscriber/Doctrine/LemonUploadEventSubscriber.php
@@ -65,36 +65,38 @@ class LemonUploadEventSubscriber implements EventSubscriber {
         $document = $args->getEntity();
         if($document instanceof Document){
             $kind = $document->getKind();
-            if($document instanceof LemonDocument and $document->getLemonReference() == null){
+            if($document instanceof LemonDocument) {
+                if ($document->getLemonReference() == null) {
 
-                /** @var LemonWayInterface $lemon */
-                $lemon = $this->container->get('net.app.driver.lemonway.eur');
+                    /** @var LemonWayInterface $lemon */
+                    $lemon = $this->container->get('net.app.driver.lemonway.eur');
 
-                $resp = $lemon->callService(
-                    'UploadFile',
-                    [
-                        'wallet' => $document->getAccount()->getCif(),
-                        'fileName' => sprintf("doctype_%d.jpg", $kind->getLemonDoctype()),
-                        'type' => sprintf("%d", $kind->getLemonDoctype()),
-                        'buffer' => base64_encode(file_get_contents($document->getContent()))
-                    ]
-                );
-
-                if(is_array($resp))
-                    throw new AppException(
-                        400,
-                        "LW error",
+                    $resp = $lemon->callService(
+                        'UploadFile',
                         [
-                            'property' => 'lemonway_error - UPLOADFILE - ' . $resp['UPLOADFILE']['ERROR'],
-                            'message' => $resp['UPLOADFILE']['MESSAGE']
+                            'wallet' => $document->getAccount()->getCif(),
+                            'fileName' => sprintf("doctype_%d.jpg", $kind->getLemonDoctype()),
+                            'type' => sprintf("%d", $kind->getLemonDoctype()),
+                            'buffer' => base64_encode(file_get_contents($document->getContent()))
                         ]
                     );
-                if($resp->E != null)
-                    throw new AppException(400, "LW error: {$resp->E}");
 
-                if($resp->UPLOAD->ID == null)
-                    throw new AppException(503, "Bad LW response: " . json_encode($resp));
-                $document->setLemonReference($resp->UPLOAD->ID);
+                    if(is_array($resp))
+                        throw new AppException(
+                            400,
+                            "LW error",
+                            [
+                                'property' => 'lemonway_error - UPLOADFILE - ' . $resp['UPLOADFILE']['ERROR'],
+                                'message' => $resp['UPLOADFILE']['MESSAGE']
+                            ]
+                        );
+                    if($resp->E != null)
+                        throw new AppException(400, "LW error: {$resp->E}");
+
+                    if($resp->UPLOAD->ID == null)
+                        throw new AppException(503, "Bad LW response: " . json_encode($resp));
+                    $document->setLemonReference($resp->UPLOAD->ID);
+                }
             }
             elseif ($kind instanceof LemonDocumentKind) {
                 throw new AppException(400, "Cannot assign a LemonDocumentKind to Document.kind, use lemon_documents instead");
