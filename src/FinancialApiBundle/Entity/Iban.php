9d92565b71c53e85530472533985b70a7d6b3457
Lluis Santos <lluis@qbitartifacts.com>
2020-03-24 19:29:56 +0100

    fix(ibans): missed use statefultrait

--- src/FinancialApiBundle/Entity/Iban.php
+++ src/FinancialApiBundle/Entity/Iban.php
@@ -18,6 +18,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Iban extends AppObject implements Stateful, LemonObject {
 
     use LemonObjectTrait;
+    use StatefulTrait;
     const LW_STATUS_APPROVED = [5];
     const LW_STATUS_DECLINED = [1, 2, 3, 6, 7, 8, 9];
 
