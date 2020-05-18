d27c233258b1c9a8232b92fe6d74e5d72604b149
Lluis Santos <lluis@qbitartifacts.com>
2020-05-18 11:19:32 +0200

    fix(lemonway): added check for existing lw_balance before querying to lw fixes #305

--- src/FinancialApiBundle/Command/LemonwaySynchronizer/AbstractSynchronizer.php
+++ src/FinancialApiBundle/Command/LemonwaySynchronizer/AbstractSynchronizer.php
@@ -40,12 +40,17 @@ abstract class AbstractSynchronizer implements Synchronizer {
         /** @var Group $account */
         foreach ($accounts as $account){
             $this->output->writeln("[INFO] Indexing account {$account->getId()}");
-            $wid = strtoupper($account->getCif());
-            if(!$wid || strlen($wid) == 0)
-                $this->output->writeln("[WARN] CIF for account {$account->getId()} is null or empty");
+            if($account->getLwBalance() != null){
+                $wid = strtoupper($account->getCif());
+                if(!$wid || strlen($wid) == 0)
+                    $this->output->writeln("[WARN] CIF for account {$account->getId()} is null or empty");
+                else {
+                    if (!key_exists($wid, $index)) $index[$wid] = [];
+                    $index[$wid] []= $account;
+                }
+            }
             else {
-                if (!key_exists($wid, $index)) $index[$wid] = [];
-                $index[$wid] []= $account;
+                $this->output->writeln("[WARN] Null lw_balance found in {$account->getId()}, ignoring account");
             }
         }
         return $index;
