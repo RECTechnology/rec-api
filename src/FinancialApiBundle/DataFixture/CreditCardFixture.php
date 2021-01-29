f4df81297acae715dc18b33ef871f179e47536ac
iulian <iulian@qbitartifacts.com>
2021-01-29 10:50:11 +0100

    feat(CreditCardFixtures): changer external card id

--- src/FinancialApiBundle/DataFixture/CreditCardFixture.php
+++ src/FinancialApiBundle/DataFixture/CreditCardFixture.php
@@ -8,6 +8,7 @@ use App\FinancialApiBundle\Entity\CreditCard;
 use App\FinancialApiBundle\Entity\DelegatedChange;
 use App\FinancialApiBundle\Entity\DelegatedChangeData;
 use App\FinancialApiBundle\Entity\Group;
+use App\FinancialApiBundle\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -23,10 +24,11 @@ class CreditCardFixture extends Fixture implements DependentFixtureInterface {
     public function load(ObjectManager $orm)
     {
 
-        $exchanger = $orm->getRepository(Group::class)->findOneBy(['type' => Group::ACCOUNT_TYPE_ORGANIZATION]);
-        $private_accounts = $orm->getRepository(Group::class)->findBy(['type' => Group::ACCOUNT_TYPE_PRIVATE]);
+        $company = $orm->getRepository(Group::class)->findOneBy(['type' => Group::ACCOUNT_TYPE_ORGANIZATION]);
+        $user = $orm->getRepository(User::class)->findOneBy(['id' => 2]);
 
-        $this->createCreditCard($orm, $exchanger, $private_accounts[0]);
+
+        $this->createCreditCard($orm, $company, $user);
 
 
     }
@@ -34,12 +36,12 @@ class CreditCardFixture extends Fixture implements DependentFixtureInterface {
     /**
      * @param ObjectManager $orm
      */
-    private function createCreditCard(ObjectManager $orm, $exchanger, $account){
+    private function createCreditCard(ObjectManager $orm, $company, $user){
         $cc = new CreditCard();
-        $cc->setUser($account);
-        $cc->setCompany($exchanger);
+        $cc->setUser($user);
+        $cc->setCompany($company);
         $cc->setAlias('401288XXXXXX1881');
-        $cc->setExternalId(1);
+        $cc->setExternalId(304);
         $orm->persist($cc);
         $orm->flush();
         return $cc;
