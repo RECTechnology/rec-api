bd529d467d09feb73f035a87b94f1dec86eda663
Lluis Santos <lluis@ngynx.com>
2021-11-16 13:03:28 +0100

    fix(tests): removed clear cache to speedup tests (#731)
    Co-authored-by: Lluis Santos <lluis@qbitartifacts.com>

--- docker/test/docker-entrypoint.sh
+++ docker/test/docker-entrypoint.sh
@@ -4,12 +4,10 @@ set -e
 case $1 in
   test)
     composer install --no-interaction
-    bin/console --env=test cache:clear
     vendor/bin/phpunit
     ;;
   coverage)
     composer install --no-interaction > /dev/null 2>&1
-    bin/console --env=test cache:clear > /dev/null 2>&1
     vendor/bin/phpunit --coverage-text 2> /dev/null | grep -A 3 "Summary:" | grep "Lines" | awk '{print $3}' | sed -E 's/\.[0-9]*%//g'
     ;;
   *)
