99aa1737531ac3d80783f4e59c2c04dfaff610c3
Pere Andreu <Nota46@users.noreply.github.com>
2021-11-24 18:38:56 +0100

    fix(kyc): created update kyc v3 for users and tests (#750)
    * fix(kyc): testing endpoints
    
    * fix(kyc): create v3 update endpoint available for users and added some tests
    
    * fix(kyc): reformat code
    
    Co-authored-by: diegomarne <diegomtz.dev@gmail.com>

---
+++ tests/FinancialApiBundle/Admin/Users/KycTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Test\FinancialApiBundle\Admin\Users;
+
+use App\FinancialApiBundle\DataFixture\UserFixture;
+use App\FinancialApiBundle\Entity\User;
+use Test\FinancialApiBundle\BaseApiTest;
+use Test\FinancialApiBundle\CrudV3WriteTestInterface;
+
+/**
+ * Class KycTest
+ * @package Test\FinancialApiBundle\Admin\Users
+ */
+class KycTest extends BaseApiTest {
+
+
+    function setUp(): void
+    {
+        parent::setUp();
+        $this->signIn(UserFixture::TEST_ADMIN_CREDENTIALS);
+    }
+
+    function testUpdateKyc()
+    {
+        $route = '/admin/v3/kyc/2';
+        $params = [
+            "street_name" => "Barraca"
+        ];
+        $resp = $this->requestJson('PUT', $route, $params);
+
+        self::assertEquals(
+            200,
+            $resp->getStatusCode(),
+            "route: $route, status_code: {$resp->getStatusCode()}, content: {$resp->getContent()}"
+        );
+
+        $content = json_decode($resp->getContent(), true);
+        self::assertArrayHasKey('data', $content);
+        $data = $content['data'];
+
+        self::assertEquals("Barraca", $data['street_name']);
+    }
+
+    function testUpdateKycs()
+    {
+        $route = '/admin/v3/kycs/2';
+        $params = [
+            "street_name" => "Barraca"
+        ];
+        $resp = $this->requestJson('PUT', $route, $params);
+
+        self::assertEquals(
+            200,
+            $resp->getStatusCode(),
+            "route: $route, status_code: {$resp->getStatusCode()}, content: {$resp->getContent()}"
+        );
+
+        $content = json_decode($resp->getContent(), true);
+        self::assertArrayHasKey('data', $content);
+        $data = $content['data'];
+
+        self::assertEquals("Barraca", $data['street_name']);
+    }
+
+}
--- tests/FinancialApiBundle/User/UserUpdateKYCTest.php
+++ tests/FinancialApiBundle/User/UserUpdateKYCTest.php
@@ -36,6 +36,69 @@ class UserUpdateKYCTest extends BaseApiTest
         self::assertRegExp("/Processing 0 mailings/", $output);
     }
 
+    function testUpdateKycV3()
+    {
+        $this->signIn(UserFixture::TEST_USER_CREDENTIALS);
+        $route = '/user/v3/kycs/1';
+        $params = [
+            "street_name" => "Barraca"
+        ];
+        $resp = $this->requestJson('PUT', $route, $params);
+
+        self::assertEquals(
+            200,
+            $resp->getStatusCode(),
+            "route: $route, status_code: {$resp->getStatusCode()}, content: {$resp->getContent()}"
+        );
+
+        $content = json_decode($resp->getContent(), true);
+        self::assertArrayHasKey('data', $content);
+
+        $resp2 = $this->requestJson('GET', "/user/v1/account");
+        $content2 = json_decode($resp2->getContent(), true);
+
+        $data = $content2['data'];
+        $kyc = $data['kyc_validations'];
+        self::assertEquals($kyc['street_name'], 'Barraca');
+    }
+
+    function testUpdateKycProtectedFieldsShouldFailV3()
+    {
+        $this->signIn(UserFixture::TEST_USER_CREDENTIALS);
+        $route = '/user/v3/kycs/1';
+        $protectedFields = [
+            "full_name_validated",
+            "email_validated",
+            "validation_phone_code",
+            "phone_validated",
+            "dateBirth_validated",
+            "document_front_status",
+            "document_rear_status",
+            "document_validated",
+            "country_validated",
+            "address_validated",
+            "proof_of_residence",
+            "tier1_status",
+            "tier2_status",
+            "tier1_status_request",
+            "tier2_status_request",
+        ];
+        foreach ($protectedFields as $field){
+            $params = [
+                $field => 1
+            ];
+            $resp = $this->requestJson('PUT', $route, $params);
+
+            self::assertEquals(
+                403,
+                $resp->getStatusCode(),
+                "route: $route, status_code: {$resp->getStatusCode()}, content: {$resp->getContent()}"
+            );
+        }
+
+    }
+
+
     function testUpdateZip(){
         $this->signIn(UserFixture::TEST_USER_CREDENTIALS);
         $resp = $this->rest(
@@ -54,4 +117,5 @@ class UserUpdateKYCTest extends BaseApiTest
 
         self::assertEquals(46500, $userInfo->kyc_validations->zip);
     }
+
 }
