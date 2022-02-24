bca930c32fa903f2c4fb8c097b0fc3485272a904
Pere Andreu <Nota46@users.noreply.github.com>
2022-02-24 15:36:33 +0100

    fix(migration): add refunded_amount to payment_order

---
+++ src/FinancialApiBundle/Migrations/Version20220222162939.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FinancialApiBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220222162939 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Add refunded_amount to payment_orders';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+         $this->addSql('ALTER TABLE PaymentOrder ADD refunded_amount VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE PaymentOrder DROP refunded_amount');
+    }
+}
