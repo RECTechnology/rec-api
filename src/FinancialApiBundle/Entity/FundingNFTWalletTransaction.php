17d676967d51d4ef9c33836c08dd19c23ae424a7
Pere Andreu <Nota46@users.noreply.github.com>
2022-07-26 12:21:02 +0200

    fix(funding): created table to store wallet nft fundings (#1125)
    Co-authored-by: diegomarne <diegomtz.dev@gmail.com>

---
+++ src/FinancialApiBundle/Entity/FundingNFTWalletTransaction.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\FinancialApiBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use JMS\Serializer\Annotation\MaxDepth;
+
+/**
+ * Class FundingNFTWalletTransaction
+ * @package App\FinancialApiBundle\Entity
+ * @ORM\Entity
+ */
+class FundingNFTWalletTransaction extends AppObject
+{
+    public const STATUS_CREATED = 'created';
+    public const STATUS_PENDING = 'pending';
+    public const STATUS_CONFIRMED = 'confirmed';
+    public const STATUS_FAILED = 'failed';
+
+    /**
+     * @ORM\Column(type="string")
+     * @Serializer\Groups({"admin"})
+     */
+    private $status;
+
+    /**
+     * This account who receive the amount
+     * @ORM\ManyToOne(targetEntity="App\FinancialApiBundle\Entity\Group")
+     * @Serializer\Groups({"admin"})
+     * @MaxDepth(1)
+     */
+    private $account;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @Serializer\Groups({"admin"})
+     */
+    private $tx_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Serializer\Groups({"admin"})
+     */
+    private $amount;
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTxId()
+    {
+        return $this->tx_id;
+    }
+
+    /**
+     * @param mixed $tx_id
+     */
+    public function setTxId($tx_id): void
+    {
+        $this->tx_id = $tx_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
+
+    /**
+     * @param mixed $account
+     */
+    public function setAccount($account): void
+    {
+        $this->account = $account;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param mixed $amount
+     */
+    public function setAmount($amount): void
+    {
+        $this->amount = $amount;
+    }
+
+}
