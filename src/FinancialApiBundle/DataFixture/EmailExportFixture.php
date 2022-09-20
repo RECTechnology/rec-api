486e8fef756c424c8e514b211533c34b5fb3d0a7
Pere Andreu <Nota46@users.noreply.github.com>
2022-09-20 11:00:55 +0200

    feat(email_exports): allow send exports to a provided email (#1162)
    Co-authored-by: diegomarne <diegomtz.dev@gmail.com>

---
+++ src/FinancialApiBundle/DataFixture/EmailExportFixture.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\FinancialApiBundle\DataFixture;
+
+use App\FinancialApiBundle\Entity\Award;
+use App\FinancialApiBundle\Entity\Badge;
+use App\FinancialApiBundle\Entity\EmailExport;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class EmailExportFixture extends Fixture
+{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $orm
+     * @throws \Exception
+     */
+    public function load(ObjectManager $orm)
+    {
+        // TODO: Implement load() method.
+        $this->createEmailExport($orm);
+    }
+
+    /**
+     * @param ObjectManager $orm
+     */
+    private function createEmailExport(ObjectManager $orm){
+
+        $export = new EmailExport();
+        $export->setStatus(EmailExport::STATUS_CREATED);
+        $export->setEntityName("User");
+        $export->setEmail("inbox@rec.qbitartifacts.com");
+        $field_map_string = '{"id": "$.id","username":"$.username","email":"$.email","locked":"$.locked","expired":"$.expired"'.
+            ',"name":"$.name","created":"$.created","dni":"$.dni","prefix":"$.prefix","phone":"$.phone"'.
+            ',"public_phone":"$.public_phone"}';
+        $query = [
+            "field_map" => $field_map_string,
+            "limit" => 1000000
+        ];
+        $export->setQuery($query);
+
+        $fieldMap = [
+            "id" => "$.id",
+            "username" => "$.username",
+            "email" => "$.email",
+            "locked" => "$.locked",
+            "expired" => "$.expired",
+            "name" => "$.name",
+            "dni" => "$.dni",
+            "prefix" => "$.prefix",
+            "phone" => "$.phone",
+            "public_phone" => "$.public_phone",
+        ];
+        $export->setFieldMap($fieldMap);
+
+        $orm->persist($export);
+        $orm->flush();
+    }
+
+}
