9a22db7f408f918c39e1797acab2451cb8839dde
Pere Andreu <Nota46@users.noreply.github.com>
2023-02-06 10:11:16 +0100

    feat(challenges): add totals for statistic things migration file (#1301)
    Co-authored-by: diegomarne <diegomtz.dev@gmail.com>

---
+++ src/FinancialApiBundle/Migrations/Version20230201130413.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FinancialApiBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230201130413 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Add totals to account challenges';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+
+        $this->addSql('ALTER TABLE AccountChallenge ADD total_amount BIGINT DEFAULT NULL, ADD total_transactions INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+
+        $this->addSql('ALTER TABLE AccountChallenge DROP total_amount, DROP total_transactions');
+
+    }
+}
