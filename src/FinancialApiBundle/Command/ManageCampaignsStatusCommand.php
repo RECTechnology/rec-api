e6cdc280bcd0a689b8ee31eeef1776a89c1dafe3
Pere Andreu <Nota46@users.noreply.github.com>
2023-02-09 11:34:17 +0100

    fix(campaigns): fixed manage status cron (#1311)
    Co-authored-by: diegomarne <diegomtz.dev@gmail.com>

--- src/FinancialApiBundle/Command/ManageCampaignsStatusCommand.php
+++ src/FinancialApiBundle/Command/ManageCampaignsStatusCommand.php
@@ -11,18 +11,18 @@ class ManageCampaignsStatusCommand extends SynchronizedContainerAwareCommand
 
     protected function configure(){
         $this->setName('rec:manage:campaigns')
-            ->setDescription('manage campaigns status. Open and close campaigns automatically');
+            ->setDescription('manage campaigns status. Open and close v2 campaigns automatically');
     }
     protected function executeSynchronized(InputInterface $input, OutputInterface $output)
     {
         // TODO: Implement executeSynchronized() method.
         $em = $this->getContainer()->get('doctrine')->getManager();
-        $campaigns_v2 = $em->getRepository(Campaign::class)->findBy(array('status' => [Campaign::STATUS_CREATED, Campaign::STATUS_ACTIVE], 'version' => 2));
+        $campaigns_v2 = $em->getRepository(Campaign::class)->findBy(array('version' => 2));
         $now = new \DateTime();
         $output->writeln(count($campaigns_v2).' campaigns found to manage');
         /** @var Campaign $campaign */
         foreach ($campaigns_v2 as $campaign){
-            if($campaign->getStatus() === Campaign::STATUS_CREATED && $campaign->getInitDate() >= $now){
+            if($campaign->getStatus() === Campaign::STATUS_CREATED && $campaign->getInitDate() <= $now){
                 $campaign->setStatus(Campaign::STATUS_ACTIVE);
                 $em->flush();
                 $output->writeln('Campaign '.$campaign->getName().' is active now');
@@ -30,6 +30,10 @@ class ManageCampaignsStatusCommand extends SynchronizedContainerAwareCommand
                 $campaign->setStatus(Campaign::STATUS_FINISHED);
                 $em->flush();
                 $output->writeln('Campaign '.$campaign->getName().' is finished now');
+            }elseif ($campaign->getStatus() === Campaign::STATUS_FINISHED && $campaign->getEndDate() > $now){
+                $campaign->setStatus(Campaign::STATUS_ACTIVE);
+                $em->flush();
+                $output->writeln('Campaign '.$campaign->getName().' is reactivated now');
             }
         }
     }
