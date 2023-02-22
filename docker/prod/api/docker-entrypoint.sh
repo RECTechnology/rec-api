faf930d0adb0597a5394b59553cfd08058fa171f
Lluis Santos <lluis@qbitartifacts.com>
2023-02-22 11:30:33 +0100

    fix(docker): fix generating .env.local from env

--- docker/prod/api/docker-entrypoint.sh
+++ docker/prod/api/docker-entrypoint.sh
@@ -7,7 +7,12 @@ APP_VERSION=$(git describe --tags)
 export APP_VERSION
 
 envsubst < config/parameters-docker.yml.dist > config/parameters.yml
-env | grep -v "^_" > .env.local
+
+env | grep -v "^_" | while read envvar; do
+  name=$(cut -f1 -d= <<<$envvar)
+  value=$(cut -f2- -d= <<<$envvar)
+  echo "$name='$value'" >> .env.local
+done
 
 composer run-script post-update-cmd
 
