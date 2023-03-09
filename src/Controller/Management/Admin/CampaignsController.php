40d264cf3037822b7507f93c2fc1b19c9cd84d43
Pere Andreu <Nota46@users.noreply.github.com>
2023-03-09 10:49:25 +0100

    fix(campaigns): allowed to search users in campaigns for admins (#1352)
    Co-authored-by: diegomarne <diegomtz.dev@gmail.com>

--- src/Controller/Management/Admin/CampaignsController.php
+++ src/Controller/Management/Admin/CampaignsController.php
@@ -4,12 +4,15 @@ namespace App\Controller\Management\Admin;
 
 use App\Entity\AccountCampaign;
 use App\Entity\Campaign;
+use App\Entity\Group;
 use DateTimeZone;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use App\Controller\BaseApiController;
 use App\Entity\User;
+use function PHPUnit\Framework\isEmpty;
 
 /**
  * Class CampaignsController
@@ -24,7 +27,7 @@ class CampaignsController extends BaseApiController{
         return new Campaign();
     }
 
-    public function listUsersByCampaign($id){
+    public function listUsersByCampaign(Request $request,$id){
         $admin_user = $this->get('security.token_storage')->getToken()->getUser();
         if(!$admin_user->hasRole('ROLE_SUPER_ADMIN')) throw new HttpException(403, 'You don\'t have the necessary permissions');
 
@@ -32,6 +35,79 @@ class CampaignsController extends BaseApiController{
         $campaign = $this->getRepository()->find($id);
 
         if(!$campaign) throw new HttpException(404, 'Campaign not found');
+
+        if($request->query->has('search')){
+            $search = $request->query->get('search');
+            $em = $this->get('doctrine.orm.entity_manager');
+            $qb = $em->createQueryBuilder();
+            $qb->from(User::class, 'u');
+
+            $fields = array(
+                'username',
+                'phone',
+                'name'
+            );
+
+            $searchFilter = $qb->expr()->orX();
+            foreach ($fields as $field){
+                $searchFilter->add(
+                    $qb->expr()->like(
+                        'u.' . $field,
+                        $qb->expr()->literal('%' . $search . '%')
+                    )
+                );
+            }
+            $where = $qb->expr()->andX();
+            $where->add($searchFilter);
+
+            $users = $qb->select('u')
+                ->where($where)
+                ->getQuery()
+                ->getResult();
+
+            $total_accumulated_bonus = 0;
+            $total_spent_bonus = 0;
+            $response_users = [];
+            if(count($users) > 0){
+                foreach ($users as $user){
+                    $user_account_campaigns = $this->searchByUserCampaign($user, $campaign);
+                    if(count($user_account_campaigns) > 0){
+                        $userData = array(
+                            'id' => $user->getId(),
+                            'name' => $user->getKycValidations()->getName() .' '. $user->getKycValidations()->getLastName(),
+                            'username' => $user->getUsername(),
+                            'phone' => $user->getPhone(),
+                            'accumulated_bonus' => 0,
+                            'spent_bonus' => 0
+                        );
+
+                        /** @var AccountCampaign $user_account_campaign */
+                        foreach ($user_account_campaigns as $user_account_campaign){
+                            $total_spent_bonus+= $user_account_campaign->getSpentBonus();
+                            $total_accumulated_bonus += $user_account_campaign->getAcumulatedBonus();
+                            $userData['accumulated_bonus'] += $user_account_campaign->getAcumulatedBonus();
+                            $userData['spent_bonus'] += $user_account_campaign->getSpentBonus();
+                        }
+                        $response_users[] = $userData;
+                    }
+
+                }
+
+            }
+            return $this->rest(
+                self::HTTP_STATUS_CODE_OK,
+                "ok",
+                "Request successful",
+                array(
+                    'total' => count($response_users),
+                    'total_accumulated_bonus' => $total_accumulated_bonus,
+                    'total_spent_bonus' => $total_spent_bonus,
+                    'elements' => $response_users
+                )
+            );
+
+        }
+
 
         [$response_users, $total_accumulated_bonus, $total_spent_bonus] = $this->getListUsersByCampaign($campaign);
 
@@ -124,6 +200,24 @@ class CampaignsController extends BaseApiController{
         }
 
         return [$response_users, $total_accumulated_bonus, $total_spent_bonus];
+    }
+
+    private function searchByUserCampaign($user, $campaign){
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $accounts = $user->getGroups();
+        $account_campaigns = [];
+        /** @var Group $account */
+        foreach ($accounts as $account){
+            if($account->getKycManager() === $user){
+                $account_campaign = $em->getRepository(AccountCampaign::class)->findOneBy(array('account' => $account, 'campaign' => $campaign));
+                if($account_campaign){
+                    $account_campaigns[] = $account_campaign;
+                }
+            }
+        }
+
+        return $account_campaigns;
     }
 
 }
